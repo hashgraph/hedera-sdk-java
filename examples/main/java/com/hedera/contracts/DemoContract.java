6ecad5bf3a3d5e50b6d0e0bd937ece35a9d907f1
Greg Scullard <greg@scullard.com>
2018-12-11 10:34:35 +0100

    Removed unnecessary account creation from contract demo

--- examples/main/java/com/hedera/contracts/DemoContract.java
+++ examples/main/java/com/hedera/contracts/DemoContract.java
@@ -29,83 +29,66 @@ public final class DemoContract {
 		HederaTransactionAndQueryDefaults txQueryDefaults = new HederaTransactionAndQueryDefaults();
 		txQueryDefaults = ExampleUtilities.getTxQueryDefaults();
 
-		// create an account
-		// new account object
-		HederaAccount account = new HederaAccount();
+		// set the file's ACL to the account's keypair
+		txQueryDefaults.fileWacl = txQueryDefaults.payingKeyPair;
+
+		// create a file
+		// new file object
+		HederaFile file = new HederaFile();
 		// setup transaction/query defaults (durations, etc...)
-		account.txQueryDefaults = txQueryDefaults;
+		file.txQueryDefaults = txQueryDefaults;
 
-		// create an account
-		HederaCryptoKeyPair newAccountKey = new HederaCryptoKeyPair(KeyType.ED25519);
-		account = AccountCreate.create(account, newAccountKey, 60000);
-		
-		if (account != null) {
-			// the paying account is now the new account
-			txQueryDefaults.payingAccountID = account.getHederaAccountID();
-			txQueryDefaults.payingKeyPair = newAccountKey;
-			txQueryDefaults.fileWacl = newAccountKey;
+		// get file contents
+		InputStream is = DemoContract.class.getResourceAsStream("/main/resources/simpleStorage.bin");
+	    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	    int nRead;
+	    byte[] data = new byte[4096];
+	    while ((nRead = is.read(data, 0, data.length)) != -1) {
+	        buffer.write(data, 0, nRead);
+	    }
+	 
+	    buffer.flush();
+	    byte[] fileContents = buffer.toByteArray();
+	    
+		// create a file with contents
+		file = FileCreate.create(file, fileContents);
 
-			// create a file
-			// new file object
-			HederaFile file = new HederaFile();
-			// setup transaction/query defaults (durations, etc...)
-			file.txQueryDefaults = txQueryDefaults;
+		// new contract object
+		HederaContract contract = new HederaContract();
+		// setup transaction/query defaults (durations, etc...)
+		contract.txQueryDefaults = txQueryDefaults;
 
-			// get file contents
-			InputStream is = DemoContract.class.getResourceAsStream("/main/resources/simpleStorage.bin");
-		    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		    int nRead;
-		    byte[] data = new byte[4096];
-		    while ((nRead = is.read(data, 0, data.length)) != -1) {
-		        buffer.write(data, 0, nRead);
-		    }
-		 
-		    buffer.flush();
-		    byte[] fileContents = buffer.toByteArray();
-		    
-			// create a file with contents
-			file = FileCreate.create(file, fileContents);
-
-			// new contract object
-			HederaContract contract = new HederaContract();
-			// setup transaction/query defaults (durations, etc...)
-			contract.txQueryDefaults = txQueryDefaults;
-
-			// create a contract
-			contract = ContractCreate.create(contract, file.getFileID(), 0);
-			//contract = create(contract, file.getFileID(), 1);
+		// create a contract
+		contract = ContractCreate.create(contract, file.getFileID(), 0);
+		//contract = create(contract, file.getFileID(), 1);
+		if (contract != null) {
+			// update the contract
+			HederaTimeStamp expirationTime = new HederaTimeStamp(100, 10);
+			HederaDuration autoRenewDuration = new HederaDuration(10, 20);
+			
+			contract = ContractUpdate.update(contract, expirationTime, autoRenewDuration);
+			
 			if (contract != null) {
-				// update the contract
-				HederaTimeStamp expirationTime = new HederaTimeStamp(100, 10);
-				HederaDuration autoRenewDuration = new HederaDuration(10, 20);
-				
-				contract = ContractUpdate.update(contract, expirationTime, autoRenewDuration);
-				
-				if (contract != null) {
-					// getinfo
-					ContractGetInfo.getInfo(contract);
-					// get bytecode
+				// getinfo
+				ContractGetInfo.getInfo(contract);
+				// get bytecode
 //					ContractGetBytecode.getByteCode(contract);
-					// call
-					final String SC_SET_ABI = "{\"constant\":false,\"inputs\":[{\"name\":\"x\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}";
-					long gas = 250000;
-					long amount = 14;
-					byte[] functionParameters = SoliditySupport.encodeSet(10,SC_SET_ABI);
-					
-					ContractCall.call(contract, gas, amount, functionParameters);
-					// call local
-					String SC_GET_ABI = "{\"constant\":true,\"inputs\":[],\"name\":\"get\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}";
-					
-					byte[] function = SoliditySupport.encodeGetValue(SC_GET_ABI);
-					long localGas = 250000;
-					long maxResultSize = 5000;
-					HederaContractFunctionResult functionResult = ContractRunLocal.runLocal(contract, localGas, maxResultSize, function);
-					int decodeResult = SoliditySupport.decodeGetValueResult(functionResult.contractCallResult(),SC_GET_ABI);
-					logger.info(String.format("===>Decoded functionResult= %d", decodeResult));
-					
-					long balance = account.getBalance();
-					logger.info(String.format("Account balance is %d", balance));
-				}
+				// call
+				final String SC_SET_ABI = "{\"constant\":false,\"inputs\":[{\"name\":\"x\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}";
+				long gas = 250000;
+				long amount = 14;
+				byte[] functionParameters = SoliditySupport.encodeSet(10,SC_SET_ABI);
+				
+				ContractCall.call(contract, gas, amount, functionParameters);
+				// call local
+				String SC_GET_ABI = "{\"constant\":true,\"inputs\":[],\"name\":\"get\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}";
+				
+				byte[] function = SoliditySupport.encodeGetValue(SC_GET_ABI);
+				long localGas = 250000;
+				long maxResultSize = 5000;
+				HederaContractFunctionResult functionResult = ContractRunLocal.runLocal(contract, localGas, maxResultSize, function);
+				int decodeResult = SoliditySupport.decodeGetValueResult(functionResult.contractCallResult(),SC_GET_ABI);
+				logger.info(String.format("===>Decoded functionResult= %d", decodeResult));
 			}
 		}
 	}
