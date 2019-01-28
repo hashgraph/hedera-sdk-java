024e3a7be6027fdb33ec542fd97a1d0fdcd4893d
Greg Scullard <greg@scullard.com>
2019-01-28 12:36:28 +0100

    Renamed solidity files to .sol

--- examples/src/main/resources/scExamples/simpleStorage.sol
+++ examples/src/main/resources/scExamples/simpleStorage.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.5.3;
+
 contract SimpleStorage {
     uint storedData;
+
     function set(uint x) public {
         storedData = x;
     }
+
     function get() public view returns (uint) {
         return storedData;
     }
