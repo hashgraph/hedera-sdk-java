294065409a920db1113493df2ee8eecf737e79b2
Ryan Leckey <ryan@launchbadge.com>
2019-03-14 02:22:03 -0700

    Initial commit

---
+++ src/main/java/com/hedera/sdk/Library.java
@@ -0,0 +1,7 @@
+package com.hedera.sdk;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/groovy/com/hedera/sdk/LibraryTest.groovy
@@ -0,0 +1,16 @@
+package com.hedera.sdk
+
+import spock.lang.Specification
+
+class LibraryTest extends Specification {
+    def "someLibraryMethod returns true"() {
+        setup:
+        def lib = new Library()
+
+        when:
+        def result = lib.someLibraryMethod()
+
+        then:
+        result
+    }
+}
