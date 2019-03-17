ae166ec6c54bcac7766393dd34e9a12e461fd0fc
Ryan Leckey <ryan@launchbadge.com>
2019-03-16 22:20:51 -0700

    Add basic AccountId class

---
+++ src/main/java/com/hedera/sdk/AccountId.java
@@ -0,0 +1,30 @@
+package com.hedera.sdk;
+
+import com.hedera.sdk.proto.AccountID;
+
+public class AccountId {
+  AccountID.Builder inner;
+
+  public AccountId(long accountNum) {
+    this(0, 0, accountNum);
+  }
+
+  public AccountId(long shardNum, long realmNum, long accountNum) {
+    inner = AccountID.newBuilder()
+      .setRealmNum(realmNum)
+      .setShardNum(shardNum)
+      .setAccountNum(accountNum);
+  }
+
+  public long getShardNum() {
+    return inner.getShardNum();
+  }
+
+  public long getRealmNum() {
+    return inner.getRealmNum();
+  }
+
+  public long getAccountNum() {
+    return inner.getAccountNum();
+  }
+}
