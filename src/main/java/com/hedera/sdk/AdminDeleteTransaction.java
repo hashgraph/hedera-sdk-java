4b62def5f754cee92760489ac58df6cfb34bb832
Kristy Brambila <kristy@launchbadge.com>
2019-03-21 12:05:25 -0700

    Update AdminDeleteTransaction

--- src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
@@ -3,19 +3,19 @@ package com.hedera.sdk;
 import com.hedera.sdk.proto.ContractID;
 import javax.annotation.Nonnull;
 
-public class AdminDeleteTransaction extends TransactionBodyBuilder<AdminDeleteTransaction> {
+public class AdminDeleteTransaction extends TransactionBuilder<AdminDeleteTransaction> {
     // Transaction to delete a File
     public AdminDeleteTransaction(@Nonnull FileId fileID) {
-        inner.getAdminDeleteBuilder().setFileID(fileID.inner);
+        inner.getBodyBuilder().getAdminDeleteBuilder().setFileID(fileID.inner);
     }
 
     // Transaction to delete a contract
     public AdminDeleteTransaction(@Nonnull ContractID contractID) {
-        inner.getAdminDeleteBuilder().setContractID(contractID);
+        inner.getBodyBuilder().getAdminDeleteBuilder().setContractID(contractID);
     }
 
     public final AdminDeleteTransaction setExpirationTime(@Nonnull TimestampSeconds timestamp) {
-        inner.getAdminDeleteBuilder().setExpirationTime(timestamp.inner);
+        inner.getBodyBuilder().getAdminDeleteBuilder().setExpirationTime(timestamp.inner);
         return this;
     }
 }
