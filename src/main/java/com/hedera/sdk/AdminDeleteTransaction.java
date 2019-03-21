0b63c6fd39ca86bc93f7e6fa51a7985d43cc8b5a
Kristy Brambila <kristy@launchbadge.com>
2019-03-21 14:46:08 -0700

    Make AdminDeleteTransaction class final

--- src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
@@ -2,18 +2,18 @@ package com.hedera.sdk;
 
 import java.time.Instant;
 
-public class AdminDeleteTransaction extends TransactionBuilder<AdminDeleteTransaction> {
-    public final AdminDeleteTransaction setID(FileId fileId) {
+public final class AdminDeleteTransaction extends TransactionBuilder<AdminDeleteTransaction> {
+    public AdminDeleteTransaction setID(FileId fileId) {
         inner.getBodyBuilder().getAdminDeleteBuilder().setFileID(fileId.inner);
         return this;
     }
 
-    public final AdminDeleteTransaction setID(ContractId contractId) {
+    public AdminDeleteTransaction setID(ContractId contractId) {
         inner.getBodyBuilder().getAdminDeleteBuilder().setContractID(contractId.inner);
         return this;
     }
 
-    public final AdminDeleteTransaction setExpirationTime(Instant timestamp) {
+    public AdminDeleteTransaction setExpirationTime(Instant timestamp) {
         inner.getBodyBuilder()
                 .getAdminDeleteBuilder()
                 .setExpirationTime(TimestampHelper.timestampSecondsFrom(timestamp));
