b44a106a3b3447f7ff99377ad29311a5e085c19e
Kristy Brambila <kristy@launchbadge.com>
2019-03-21 14:47:25 -0700

    Make AdminUndeleteTransaction class final instead of individual methods being final

--- src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
@@ -1,12 +1,12 @@
 package com.hedera.sdk;
 
-public class AdminUndeleteTransaction extends TransactionBuilder<AdminUndeleteTransaction> {
-    public final AdminUndeleteTransaction setID(FileId fileId) {
+public final class AdminUndeleteTransaction extends TransactionBuilder<AdminUndeleteTransaction> {
+    public AdminUndeleteTransaction setID(FileId fileId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setFileID(fileId.inner);
         return this;
     }
 
-    public final AdminUndeleteTransaction setID(ContractId contractId) {
+    public AdminUndeleteTransaction setID(ContractId contractId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setContractID(contractId.inner);
         return this;
     }
