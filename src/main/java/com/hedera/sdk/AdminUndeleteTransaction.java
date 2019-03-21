fe4e1c0abfd0a2835a97cbfeac62a201b0fd9640
Kristy Brambila <kristy@launchbadge.com>
2019-03-21 14:31:13 -0700

    Remove unnecessary non null

--- src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
@@ -1,14 +1,12 @@
 package com.hedera.sdk;
 
-import javax.annotation.Nonnull;
-
 public class AdminUndeleteTransaction extends TransactionBuilder<AdminUndeleteTransaction> {
-    public final AdminUndeleteTransaction setID(@Nonnull FileId fileId) {
+    public final AdminUndeleteTransaction setID(FileId fileId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setFileID(fileId.inner);
         return this;
     }
 
-    public final AdminUndeleteTransaction setID(@Nonnull ContractId contractId) {
+    public final AdminUndeleteTransaction setID(ContractId contractId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setContractID(contractId.inner);
         return this;
     }
