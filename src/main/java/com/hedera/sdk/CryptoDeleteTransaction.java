627d0de9b53961f9b36ed7939a94a4662b0d9d8a
Daniel Akhterov <daniel@launchbadge.com>
2019-03-26 15:32:07 -0700

    Use builder pattern

--- src/main/java/com/hedera/sdk/CryptoDeleteTransaction.java
+++ src/main/java/com/hedera/sdk/CryptoDeleteTransaction.java
@@ -4,11 +4,18 @@ import javax.annotation.Nonnull;
 
 public class CryptoDeleteTransaction
         extends TransactionBuilder<com.hedera.sdk.CryptoDeleteTransaction> {
-    public CryptoDeleteTransaction(
-            @Nonnull AccountId transferAccountId, @Nonnull AccountId deleteAccountId) {
+
+    public CryptoDeleteTransaction() {}
+
+    public CryptoDeleteTransaction setTransferAccountId(@Nonnull AccountId transferAccountId) {
         inner.getBodyBuilder()
                 .getCryptoDeleteBuilder()
-                .setTransferAccountID(transferAccountId.inner)
-                .setDeleteAccountID(deleteAccountId.inner);
+                .setTransferAccountID(transferAccountId.inner);
+        return this;
+    }
+
+    public CryptoDeleteTransaction setDeleteAccountId(@Nonnull AccountId deleteAccountId) {
+        inner.getBodyBuilder().getCryptoDeleteBuilder().setDeleteAccountID(deleteAccountId.inner);
+        return this;
     }
 }
