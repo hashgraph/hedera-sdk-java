0f2deb7a16a83e1908e916073a0cd7b1746040ff
Josh <josh@launchbadge.com>
2019-03-26 19:07:35 -0700

    adds the setNewRealmAdminKeys functions

--- src/main/java/com/hedera/sdk/FileCreateTransaction.java
+++ src/main/java/com/hedera/sdk/FileCreateTransaction.java
@@ -4,10 +4,7 @@ import com.google.protobuf.ByteString;
 import com.hedera.sdk.crypto.IPublicKey;
 import com.hedera.sdk.proto.FileCreateTransactionBody;
 import com.hedera.sdk.proto.KeyList;
-import com.hedera.sdk.proto.Timestamp;
-import java.time.Duration;
 import java.time.Instant;
-import javax.annotation.Nonnull;
 
 public final class FileCreateTransaction extends TransactionBuilder<FileCreateTransaction> {
     private final FileCreateTransactionBody.Builder builder;
@@ -35,6 +32,18 @@ public final class FileCreateTransaction extends TransactionBuilder<FileCreateTr
 
     public FileCreateTransaction setContents(byte[] bytes) {
         builder.setContents(ByteString.copyFrom(bytes));
+        return this;
+    }
+
+    public FileCreateTransaction setNewRealmAdminKey(IPublicKey key) {
+        builder.setNewRealmAdminKey(key.toProtoKey());
+
+        return this;
+    }
+
+    public FileCreateTransaction setNewRealmAdminKey(ContractId key) {
+        builder.setNewRealmAdminKey(key.toProtoKey());
+
         return this;
     }
 }
