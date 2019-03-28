c12f698cb7743d695d34c685460d5ae0b916cb53
Kristy Brambila <kristy@launchbadge.com>
2019-03-28 13:03:57 -0700

    Add new QueryBuilder stuff to CryptoGetInfoQuery

--- src/main/java/com/hedera/sdk/CryptoGetInfoQuery.java
+++ src/main/java/com/hedera/sdk/CryptoGetInfoQuery.java
@@ -1,11 +1,13 @@
 package com.hedera.sdk;
 
-import com.hedera.sdk.proto.QueryHeader;
+import com.hedera.sdk.proto.*;
+import io.grpc.MethodDescriptor;
 
-public final class CryptoGetInfoQuery extends QueryBuilder {
+public final class CryptoGetInfoQuery extends QueryBuilder<CryptoGetInfoResponse> {
     private final com.hedera.sdk.proto.CryptoGetInfoQuery.Builder builder;
 
     public CryptoGetInfoQuery() {
+        super(Response::getCryptoGetInfo);
         builder = inner.getCryptoGetInfoBuilder();
     }
 
@@ -17,5 +19,10 @@ public final class CryptoGetInfoQuery extends QueryBuilder {
     public CryptoGetInfoQuery setAccount(AccountId account) {
         builder.setAccountID(account.inner);
         return this;
+    }
+
+    @Override
+    MethodDescriptor<Query, Response> getMethod() {
+        return CryptoServiceGrpc.getGetAccountInfoMethod();
     }
 }
