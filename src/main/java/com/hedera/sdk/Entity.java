a5c896d853718dee28d6f216e8c5aa718b70db02
Josh <josh@launchbadge.com>
2019-03-27 22:11:30 -0700

    Adds EntityId

---
+++ src/main/java/com/hedera/sdk/Entity.java
@@ -0,0 +1,50 @@
+package com.hedera.sdk;
+
+import com.hedera.sdk.proto.EntityID;
+
+public class InvalidEntityRequestException extends Exception {
+    public InvalidEntityRequestException(String message) {
+        super(message);
+    }
+};
+
+public final class Entity {
+
+    private final EntityID.Builder inner;
+
+    public Entity(EntityID proto) {
+        inner = proto.toBuilder();
+    }
+
+    public AccountId getAccount() throws InvalidEntityRequestException {
+        if (!inner.hasAccountID()) {
+            throw new InvalidEntityRequestException("Does not contain AccountId");
+        }
+
+        var raw = inner.getAccountID();
+
+        return new AccountId(raw.getShardNum(), raw.getRealmNum(), raw.getAccountNum());
+    }
+
+    // todo: Claim
+
+    public FileId getFile() throws InvalidEntityRequestException {
+        if (!inner.hasFileID()) {
+            throw new InvalidEntityRequestException("Does not contain FileId");
+        }
+
+        var raw = inner.getFileID();
+
+        return new FileId(raw.getShardNum(), raw.getRealmNum(), raw.getFileNum());
+    }
+
+    public ContractId getContract() throws InvalidEntityRequestException {
+        if (!inner.hasContractID()) {
+            throw new InvalidEntityRequestException("Does not contain ContractId");
+        }
+
+        var raw = inner.getContractID();
+
+        return new ContractId(raw.getShardNum(), raw.getRealmNum(), raw.getContractNum());
+    }
+}
