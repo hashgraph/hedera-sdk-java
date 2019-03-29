f43e82d34c9bc580b5a40f2b570d96d4f76f66dc
Austin Bonander <austin@launchbadge.com>
2019-03-29 15:58:20 -0700

    restore `Admin[Un]DeleteTransaction`

--- src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminDeleteTransaction.java
@@ -1,29 +1,23 @@
-package com.hedera.sdk.file;
+package com.hedera.sdk;
 
-import com.hedera.sdk.ContractId;
-import com.hedera.sdk.FileId;
-import com.hedera.sdk.TimestampHelper;
-import com.hedera.sdk.TransactionBuilder;
 import com.hedera.sdk.proto.FileServiceGrpc;
 import com.hedera.sdk.proto.Transaction;
 import com.hedera.sdk.proto.TransactionResponse;
 import io.grpc.MethodDescriptor;
 import java.time.Instant;
 
-// `AdminDeleteTransaction`
-public final class FileAdminDeleteTransaction
-        extends TransactionBuilder<FileAdminDeleteTransaction> {
-    public FileAdminDeleteTransaction setID(FileId fileId) {
+public final class AdminDeleteTransaction extends TransactionBuilder<AdminDeleteTransaction> {
+    public AdminDeleteTransaction setID(FileId fileId) {
         inner.getBodyBuilder().getAdminDeleteBuilder().setFileID(fileId.toProto());
         return this;
     }
 
-    public FileAdminDeleteTransaction setID(ContractId contractId) {
+    public AdminDeleteTransaction setID(ContractId contractId) {
         inner.getBodyBuilder().getAdminDeleteBuilder().setContractID(contractId.toProto());
         return this;
     }
 
-    public FileAdminDeleteTransaction setExpirationTime(Instant timestamp) {
+    public AdminDeleteTransaction setExpirationTime(Instant timestamp) {
         inner.getBodyBuilder()
                 .getAdminDeleteBuilder()
                 .setExpirationTime(TimestampHelper.timestampSecondsFrom(timestamp));
--- src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
+++ src/main/java/com/hedera/sdk/AdminUndeleteTransaction.java
@@ -1,22 +1,17 @@
-package com.hedera.sdk.file;
+package com.hedera.sdk;
 
-import com.hedera.sdk.ContractId;
-import com.hedera.sdk.FileId;
-import com.hedera.sdk.TransactionBuilder;
 import com.hedera.sdk.proto.FileServiceGrpc;
 import com.hedera.sdk.proto.Transaction;
 import com.hedera.sdk.proto.TransactionResponse;
 import io.grpc.MethodDescriptor;
 
-// `AdminUndeleteTransaction`
-public final class FileAdminUndeleteTransaction
-        extends TransactionBuilder<FileAdminUndeleteTransaction> {
-    public FileAdminUndeleteTransaction setID(FileId fileId) {
+public final class AdminUndeleteTransaction extends TransactionBuilder<AdminUndeleteTransaction> {
+    public AdminUndeleteTransaction setID(FileId fileId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setFileID(fileId.toProto());
         return this;
     }
 
-    public FileAdminUndeleteTransaction setID(ContractId contractId) {
+    public AdminUndeleteTransaction setID(ContractId contractId) {
         inner.getBodyBuilder().getAdminUndeleteBuilder().setContractID(contractId.toProto());
         return this;
     }
