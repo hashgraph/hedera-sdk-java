82df0f20558064f72002ad6049c0c87183799cf7
Austin Bonander <austin@launchbadge.com>
2019-03-29 05:54:04 -0700

    `ContractCreateTransaction`: make setters public

--- src/main/java/com/hedera/sdk/contract/ContractCreateTransaction.java
+++ src/main/java/com/hedera/sdk/contract/ContractCreateTransaction.java
@@ -23,57 +23,57 @@ public class ContractCreateTransaction extends TransactionBuilder<ContractCreate
     }
 
     // more descriptive name than `setFileId`
-    ContractCreateTransaction setBytecodeFile(FileId fileId) {
+    public ContractCreateTransaction setBytecodeFile(FileId fileId) {
         builder.setFileID(fileId.toProto());
         return this;
     }
 
-    ContractCreateTransaction setAdminKey(Key adminKey) {
+    public ContractCreateTransaction setAdminKey(Key adminKey) {
         builder.setAdminKey(adminKey.toKeyProto());
         return this;
     }
 
-    ContractCreateTransaction setGas(long gas) {
+    public ContractCreateTransaction setGas(long gas) {
         builder.setGas(gas);
         return this;
     }
 
-    ContractCreateTransaction setIntialBalance(long intialBalance) {
+    public ContractCreateTransaction setIntialBalance(long intialBalance) {
         builder.setInitialBalance(intialBalance);
         return this;
     }
 
-    ContractCreateTransaction setProxyAccount(AccountId proxyAccountId) {
+    public ContractCreateTransaction setProxyAccount(AccountId proxyAccountId) {
         builder.setProxyAccountID(proxyAccountId.toProto());
         return this;
     }
 
-    ContractCreateTransaction setProxyFraction(int proxyFraction) {
+    public ContractCreateTransaction setProxyFraction(int proxyFraction) {
         builder.setProxyFraction(proxyFraction);
         return this;
     }
 
-    ContractCreateTransaction setAutoRenewPeriod(java.time.Duration duration) {
+    public ContractCreateTransaction setAutoRenewPeriod(java.time.Duration duration) {
         builder.setAutoRenewPeriod(DurationHelper.durationFrom(duration));
         return this;
     }
 
-    ContractCreateTransaction setConstructorParams(byte[] constructorParams) {
+    public ContractCreateTransaction setConstructorParams(byte[] constructorParams) {
         builder.setConstructorParameters(ByteString.copyFrom(constructorParams));
         return this;
     }
 
-    ContractCreateTransaction setShard(long shardId) {
+    public ContractCreateTransaction setShard(long shardId) {
         builder.setShardID(ShardID.newBuilder().setShardNum(shardId));
         return this;
     }
 
-    ContractCreateTransaction setRealm(long realmId) {
+    public ContractCreateTransaction setRealm(long realmId) {
         builder.setRealmID(RealmID.newBuilder().setRealmNum(realmId));
         return this;
     }
 
-    ContractCreateTransaction setNewRealmAdminKey(Key newRealmAdminKey) {
+    public ContractCreateTransaction setNewRealmAdminKey(Key newRealmAdminKey) {
         builder.setNewRealmAdminKey(newRealmAdminKey.toKeyProto());
         return this;
     }
