0c1eed4be4c55357af11a63d597e7d298270befe
Austin Bonander <austin@launchbadge.com>
2019-03-29 00:30:30 +0000

    HJS-51: implement `TransactionGetRecordQuery`
    !!: does not have a corresponding service method
    
    builds on !39

---
+++ src/main/java/com/hedera/sdk/TransactionGetReceiptQuery.java
@@ -0,0 +1,28 @@
+package com.hedera.sdk;
+
+import com.hedera.sdk.proto.*;
+import io.grpc.MethodDescriptor;
+
+public class TransactionGetReceiptQuery extends QueryBuilder<TransactionGetReceiptResponse> {
+    private final com.hedera.sdk.proto.TransactionGetReceiptQuery.Builder builder;
+
+    public TransactionGetReceiptQuery() {
+        super(Response::getTransactionGetReceipt);
+        builder = inner.getTransactionGetReceiptBuilder();
+    }
+
+    @Override
+    protected QueryHeader.Builder getHeaderBuilder() {
+        return builder.getHeaderBuilder();
+    }
+
+    public TransactionGetReceiptQuery setTransaction(TransactionId transactionId) {
+        builder.setTransactionID(transactionId.inner);
+        return this;
+    }
+
+    @Override
+    MethodDescriptor<Query, Response> getMethod() {
+        return CryptoServiceGrpc.getGetTxRecordByTxIDMethod();
+    }
+}
---
+++ src/main/java/com/hedera/sdk/TransactionGetRecordQuery.java
@@ -0,0 +1,29 @@
+package com.hedera.sdk;
+
+import com.hedera.sdk.proto.*;
+import io.grpc.MethodDescriptor;
+
+public class TransactionGetRecordQuery extends QueryBuilder<TransactionGetRecordResponse> {
+    private final com.hedera.sdk.proto.TransactionGetRecordQuery.Builder builder;
+
+    public TransactionGetRecordQuery() {
+        super(Response::getTransactionGetRecord);
+        builder = inner.getTransactionGetRecordBuilder();
+    }
+
+    @Override
+    protected QueryHeader.Builder getHeaderBuilder() {
+        return builder.getHeaderBuilder();
+    }
+
+    public TransactionGetRecordQuery setTransaction(TransactionId transaction) {
+        builder.setTransactionID(transaction.inner);
+        return this;
+    }
+
+    @Override
+    MethodDescriptor<Query, Response> getMethod() {
+        // FIXME does not have a corresponding service method
+        throw new Error("not implemented");
+    }
+}
