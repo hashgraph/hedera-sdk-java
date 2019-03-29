f4c74a9c9fc947c8cf8c7c259d4776d4af717dc5
Austin Bonander <austin@launchbadge.com>
2019-03-29 14:56:35 -0700

    restore original name of `CryptoTransferTransaction`

--- src/main/java/com/hedera/sdk/account/CryptoTransferTransaction.java
+++ src/main/java/com/hedera/sdk/account/CryptoTransferTransaction.java
@@ -7,28 +7,24 @@ import com.hedera.sdk.proto.Transaction;
 import io.grpc.MethodDescriptor;
 import javax.annotation.Nonnegative;
 
-// `CryptoTransferTransaction`
-public final class AccountBalanceTransferTransaction
-        extends TransactionBuilder<AccountBalanceTransferTransaction> {
+public final class CryptoTransferTransaction extends TransactionBuilder<CryptoTransferTransaction> {
     private CryptoTransferTransactionBody.Builder builder;
     private TransferList.Builder transferList;
 
-    public AccountBalanceTransferTransaction() {
+    public CryptoTransferTransaction() {
         builder = inner.getBodyBuilder().getCryptoTransferBuilder();
         transferList = builder.getTransfersBuilder();
     }
 
-    public AccountBalanceTransferTransaction addSender(
-            AccountId senderId, @Nonnegative long value) {
+    public CryptoTransferTransaction addSender(AccountId senderId, @Nonnegative long value) {
         return this.addTransfer(senderId, value * -1L);
     }
 
-    public AccountBalanceTransferTransaction addRecipient(
-            AccountId recipientId, @Nonnegative long value) {
+    public CryptoTransferTransaction addRecipient(AccountId recipientId, @Nonnegative long value) {
         return this.addTransfer(recipientId, value);
     }
 
-    public AccountBalanceTransferTransaction addTransfer(AccountId accountId, long value) {
+    public CryptoTransferTransaction addTransfer(AccountId accountId, long value) {
         transferList.addAccountAmounts(
                 AccountAmount.newBuilder()
                         .setAccountID(accountId.toProto())
