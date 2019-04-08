a29599920cbca2e79922a2ccfa8a055fec7b41ed
Austin Bonander <austin@launchbadge.com>
2019-04-08 10:25:37 -0700

    spotless txn query tests

--- src/test/groovy/com/hedera/sdk/TransactionFastRecordQueryTest.groovy
+++ src/test/groovy/com/hedera/sdk/TransactionFastRecordQueryTest.groovy
@@ -10,14 +10,14 @@ class TransactionFastRecordQueryTest extends Specification {
 	def privateKey = Ed25519PrivateKey.fromString("302e020100300506032b6570042204203b054fade7a2b0869c6bd4a63b7017cbae7855d12acc357bea718e2c3e805962")
 
 	def paymentTxn = new CryptoTransferTransaction()
-		.setTransactionId(new TransactionId(new AccountId(1234), Instant.parse("2019-04-05T12:00:00Z")))
-		.addSender(new AccountId(1234), 10000)
-		.addRecipient(new AccountId(3), 10000)
-		.sign(privateKey)
+	.setTransactionId(new TransactionId(new AccountId(1234), Instant.parse("2019-04-05T12:00:00Z")))
+	.addSender(new AccountId(1234), 10000)
+	.addRecipient(new AccountId(3), 10000)
+	.sign(privateKey)
 
 	def query = new TransactionFastRecordQuery()
-		.setTransaction(new TransactionId(new AccountId(1234), Instant.parse("2019-04-05T11:00:00Z")))
-		.setPayment(paymentTxn)
+	.setTransaction(new TransactionId(new AccountId(1234), Instant.parse("2019-04-05T11:00:00Z")))
+	.setPayment(paymentTxn)
 
 	def builtQuery = query.inner.build()
 
