46c8ff61bb5b00160567119726049b9e9820343d
Ryan Leckey <ryan@launchbadge.com>
2019-04-15 23:48:17 -0700

    Clean up CreateFile and GetAccountBalance examples

--- examples/src/main/java/com/hedera/sdk/examples/CreateFile.java
+++ examples/src/main/java/com/hedera/sdk/examples/CreateFile.java
@@ -1,63 +1,34 @@
 package com.hedera.sdk.examples;
 
 import com.hedera.sdk.*;
-import com.hedera.sdk.account.AccountId;
-import com.hedera.sdk.crypto.ed25519.Ed25519PrivateKey;
 import com.hedera.sdk.file.FileCreateTransaction;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
 import java.util.Objects;
 
 @SuppressWarnings("Duplicates")
 public final class CreateFile {
-    public static void main(String[] args) throws InterruptedException {
-        var env = Dotenv.load();
-
-        var operatorKey = Ed25519PrivateKey.fromString(Objects.requireNonNull(env.get("OPERATOR_SECRET")));
-
-        var network = Objects.requireNonNull(env.get("NETWORK"));
-        var node = AccountId.fromString(Objects.requireNonNull(env.get("NODE")));
-
-        var client = new Client(Map.of(node, network));
+    public static void main(String[] args) throws HederaException {
+        var client = ExampleHelper.createHederaClient();
 
         // The file is required to be a byte array,
         // you can easily use the bytes of a file instead.
         var fileContents = "Hedera hashgraph is great!".getBytes();
 
-        var txId = new TransactionId(new AccountId(2));
-
-        var tx = new FileCreateTransaction(client).setTransactionId(txId)
-            .setNodeAccountId(new AccountId(3))
-            .setExpirationTime(
-                Instant.now()
-                    .plus(Duration.ofSeconds(2592000))
+        var tx = new FileCreateTransaction(client).setExpirationTime(
+            Instant.now()
+                .plus(Duration.ofSeconds(2592000))
+        )
+            // Use the same key as the operator to "own" this file
+            .addKey(Objects.requireNonNull(client.getOperatorKey())
+                .getPublicKey()
             )
-            .addKey(operatorKey.getPublicKey())
-            .setContents(fileContents)
-            .setMemo("[hedera-sdk-java][example] CreateFile")
-            // The first signature represents the transaction payer
-            .sign(operatorKey)
-            // The second signature represents the owner of the file
-            .sign(operatorKey);
+            .setContents(fileContents);
 
-        TransactionReceipt receipt;
-        try {
-            receipt = tx.executeForReceipt();
-        } catch (HederaException e) {
-            System.out.println("Failed to create file: " + e);
-            return;
-        }
+        var receipt = tx.executeForReceipt();
+        var newFileId = Objects.requireNonNull(receipt.getFileId());
 
-        var receiptStatus = receipt.getStatus();
-
-        System.out.println("status: " + receiptStatus.toString());
-
-        var newFileId = receipt.getFileId();
-        assert newFileId != null;
-
-        System.out.println("new file num: " + newFileId.getFileNum());
+        System.out.println("file: " + newFileId);
     }
 }
--- examples/src/main/java/com/hedera/sdk/examples/GetAccountBalance.java
+++ examples/src/main/java/com/hedera/sdk/examples/GetAccountBalance.java
@@ -1,48 +1,19 @@
 package com.hedera.sdk.examples;
 
-import com.hedera.sdk.account.AccountId;
-import com.hedera.sdk.Client;
 import com.hedera.sdk.HederaException;
-import com.hedera.sdk.TransactionId;
 import com.hedera.sdk.account.AccountBalanceQuery;
-import com.hedera.sdk.account.CryptoTransferTransaction;
-import com.hedera.sdk.crypto.ed25519.Ed25519PrivateKey;
-import io.github.cdimascio.dotenv.Dotenv;
 
-import java.util.Map;
 import java.util.Objects;
 
 @SuppressWarnings("Duplicates")
 public final class GetAccountBalance {
-    public static void main(String[] args) {
-        var env = Dotenv.load();
+    public static void main(String[] args) throws HederaException {
+        var client = ExampleHelper.createHederaClient();
 
-        var operatorKey = Ed25519PrivateKey.fromString(Objects.requireNonNull(env.get("OPERATOR_SECRET")));
-        AccountId operator;
+        var query = new AccountBalanceQuery(client).setAccount(Objects.requireNonNull(client.getOperatorId()));
 
-        operator = AccountId.fromString(Objects.requireNonNull(env.get("OPERATOR")));
+        var balance = query.execute();
 
-        var network = Objects.requireNonNull(env.get("NETWORK"));
-        var node = AccountId.fromString(Objects.requireNonNull(env.get("NODE")));
-
-        var client = new Client(Map.of(node, network));
-
-        // Account balance query requires 100,000 tinybar
-        var txPayment = new CryptoTransferTransaction(client).setNodeAccountId(node)
-            .setTransactionId(new TransactionId(node))
-            .addSender(operator, 100000)
-            .addRecipient(node, 100000)
-            .sign(operatorKey);
-
-        var query = new AccountBalanceQuery(client).setAccount(operator)
-            .setPayment(txPayment);
-
-        try {
-            var balance = query.execute();
-            System.out.println(balance.toString());
-        } catch (HederaException e) {
-            System.out.println("Failed to get account balance: " + e);
-        }
+        System.out.println("balance = " + balance);
     }
-
 }
