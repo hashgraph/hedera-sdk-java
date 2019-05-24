0273d1e06ac5c87073782d8bb02ea032112bdeb9
austin <austin@launchbadge.com>
2019-05-24 16:43:48 -0700

    port `TransactionReceiptTest` to Java

---
+++ src/test/java/com/hedera/hashgraph/sdk/TransactionReceiptTest.java
@@ -0,0 +1,78 @@
+package com.hedera.hashgraph.sdk;
+
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.proto.Response;
+import com.hedera.hashgraph.sdk.proto.ResponseCodeEnum;
+import com.hedera.hashgraph.sdk.proto.TransactionGetReceiptResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TransactionReceiptTest {
+
+    @Test
+    @DisplayName("requires `TransactionGetReceipt`")
+    void requiresTransactionGetReceipt() {
+        final var response = Response.getDefaultInstance();
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new TransactionReceipt(response),
+            ""
+        );
+    }
+
+    @Test
+    @DisplayName("missing fields throw")
+    void missingFieldsThrow() {
+        final var response = Response.newBuilder()
+            .setTransactionGetReceipt(
+                TransactionGetReceiptResponse.getDefaultInstance()
+            )
+            .build();
+
+        final var receipt = new TransactionReceipt(response);
+
+        assertEquals(receipt.getStatus(), ResponseCodeEnum.OK);
+
+        assertThrows(
+            IllegalStateException.class,
+            receipt::getAccountId,
+            "receipt does not contain an account ID"
+        );
+
+        assertThrows(
+            IllegalStateException.class,
+            receipt::getContractId,
+            "receipt does not contain a contract ID"
+        );
+
+        assertThrows(
+            IllegalStateException.class,
+            receipt::getFileId,
+            "receipt does not contain a file ID"
+        );
+    }
+
+    @Test
+    @DisplayName("receipt with account ID")
+    void receiptWithAccountId() {
+        final var expectedAccountId = new AccountId(1, 2, 3);
+
+        final var response = Response.newBuilder()
+            .setTransactionGetReceipt(
+                TransactionGetReceiptResponse.newBuilder()
+                    .setReceipt(
+                        com.hedera.hashgraph.sdk.proto.TransactionReceipt.newBuilder()
+                            .setAccountID(expectedAccountId.toProto())
+                    )
+            ).build();
+
+
+        final var receipt = new TransactionReceipt(response);
+
+        assertEquals(receipt.getAccountId(), expectedAccountId);
+    }
+}
