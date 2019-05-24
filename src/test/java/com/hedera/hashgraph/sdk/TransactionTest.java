89681399586944b022263864e595818d3ef38e9f
austin <austin@launchbadge.com>
2019-05-24 13:51:44 -0700

    port `TransactionTest` to Java

---
+++ src/test/java/com/hedera/hashgraph/sdk/TransactionTest.java
@@ -0,0 +1,78 @@
+package com.hedera.hashgraph.sdk;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import com.hedera.hashgraph.sdk.file.FileId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+    static final AccountId nodeAcctId = new AccountId(0);
+    static final AccountId acctId = new AccountId(3);
+    static final Ed25519PrivateKey key1 = Ed25519PrivateKey.fromString("302e020100300506032b6570042204203b054fade7a2b0869c6bd4a63b7017cbae7855d12acc357bea718e2c3e805962");
+
+
+    static final Instant txnStartAt = Instant.parse("2019-04-18T20:50:00Z");
+    static final TransactionId txnId = new TransactionId(acctId, txnStartAt);
+
+    // a different instance for each test
+    final Transaction txn = new AdminDeleteTransaction()
+        .setTransactionId(txnId)
+        .setNodeAccountId(nodeAcctId)
+        .setID(new FileId(0, 0, 0))
+        .build();
+
+    @Test
+    @DisplayName("validate() requires a signature")
+    void validateRequiresSignature() {
+        assertThrows(
+            IllegalStateException.class,
+            txn::validate,
+            "Transaction failed validation:\n"
+                + "Transaction requires at least one signature"
+        );
+    }
+
+    @Test
+    @DisplayName("validate() accepts a single signature")
+    void validateOneSignature() {
+        assertDoesNotThrow(() -> txn.sign(key1).validate());
+    }
+
+    @Test
+    @DisplayName("validate() accepts two signatures")
+    void validateTwoSignatures() {
+        final var key2 = Ed25519PrivateKey.generate();
+
+        assertDoesNotThrow(
+            () -> txn.sign(key1).sign(key2).validate()
+        );
+    }
+
+    @Test
+    @DisplayName("validate() forbids duplicate signing keys")
+    void validateForbidsDuplicates() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> txn.sign(key1).sign(key1),
+            "transaction already signed with key: " + key1
+        );
+    }
+
+    @Test
+    @DisplayName("transaction goes to bytes and back")
+    void testSerialization() throws InvalidProtocolBufferException {
+        final var txn1Bytes = txn.sign(key1).toBytes();
+
+        final var txn2 = Transaction.fromBytes(null, txn1Bytes);
+
+        assertEquals(txn.inner.build(), txn2.inner.build());
+        assertEquals(txn.nodeAccountId, txn2.nodeAccountId);
+        assertEquals(txn.transactionId, txn2.transactionId);
+    }
+}
