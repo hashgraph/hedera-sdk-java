19c5726ab8f850c7056f6281f74bbabdf440367d
Austin Bonander <austin@launchbadge.com>
2019-06-28 17:14:39 -0700

    implement Solidity address conversions for address/contract/file IDs

---
+++ src/main/java/com/hedera/hashgraph/sdk/SolidityUtil.java
@@ -0,0 +1,93 @@
+package com.hedera.hashgraph.sdk;
+
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.contract.ContractId;
+import com.hedera.hashgraph.sdk.file.FileId;
+
+import org.bouncycastle.util.encoders.DecoderException;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.ByteBuffer;
+
+public final class SolidityUtil {
+
+    /**
+     * The length of a Solidity address in bytes.
+     */
+    public static final int ADDRESS_LEN = 20;
+    /**
+     * The length of a hexadecimal-encoded Solidity address, in ASCII characters (bytes).
+     */
+    public static final int ADDRESS_LEN_HEX = ADDRESS_LEN * 2;
+
+    private SolidityUtil() { }
+
+    public static String addressForEntity(long shardNum, long realmNum, long entityNum) {
+        if (Long.highestOneBit(shardNum) > 32) {
+            throw new IllegalArgumentException("shardNum out of 32-bit range " + shardNum);
+        }
+
+        return Hex.toHexString(
+            ByteBuffer.allocate(20)
+                .putInt((int) shardNum)
+                .putLong(realmNum)
+                .putLong(entityNum)
+                .array());
+    }
+
+    public static String addressFor(AccountId accountId) {
+        return addressForEntity(
+            accountId.getShardNum(),
+            accountId.getRealmNum(),
+            accountId.getAccountNum());
+    }
+
+    public static String addressFor(ContractId contractId) {
+        return addressForEntity(
+            contractId.getShardNum(),
+            contractId.getRealmNum(),
+            contractId.getContractNum());
+    }
+
+    public static String addressFor(FileId fileId) {
+        return addressForEntity(
+            fileId.getShardNum(),
+            fileId.getRealmNum(),
+            fileId.getFileNum());
+    }
+
+    public static <T> T parseAddress(String address, WithAddress<T> withAddress) {
+        return decodeAddress(decodeAddress(address), withAddress);
+    }
+
+    public static <T> T decodeAddress(byte[] address, WithAddress<T> withAddress) {
+        checkAddressLen(address);
+        final var buf = ByteBuffer.wrap(address);
+        return withAddress.apply(buf.getInt(), buf.getLong(), buf.getLong());
+    }
+
+    static void checkAddressLen(byte[] address) {
+        if (address.length != ADDRESS_LEN) {
+            throw new IllegalArgumentException(
+                "Solidity addresses must be 20 bytes or 40 hex chars");
+        }
+    }
+
+    static byte[] decodeAddress(String address) {
+        if (address.length() != ADDRESS_LEN_HEX) {
+            throw new IllegalArgumentException(
+                "Solidity addresses must be 20 bytes or 40 hex chars");
+        }
+
+        try {
+            return Hex.decode(address);
+        } catch (DecoderException e) {
+            throw new IllegalArgumentException("failed to decode Solidity address as hex", e);
+        }
+    }
+
+    @FunctionalInterface
+    public interface WithAddress<T> {
+        T apply(long shardNum, long realmNum, long entityNum);
+    }
+}
