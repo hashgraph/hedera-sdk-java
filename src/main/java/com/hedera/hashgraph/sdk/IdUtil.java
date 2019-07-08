f3345c81c1278548c13921f0ce50557a8a3b5672
cylon56 <michaelnlewellen@gmail.com>
2019-07-08 16:37:17 -0500

    fixed 2 checkstyle violations

--- src/main/java/com/hedera/hashgraph/sdk/IdUtil.java
+++ src/main/java/com/hedera/hashgraph/sdk/IdUtil.java
@@ -3,12 +3,15 @@ package com.hedera.hashgraph.sdk;
 import com.google.common.base.Splitter;
 
 public final class IdUtil {
+
+    private IdUtil() {  }
+
     public static long[] parseIdString(String id) {
         var rawNums = Splitter.on('.')
             .split(id)
             .iterator();
 
-       return new long[] {
+        return new long[] {
             Long.parseLong(rawNums.next()),
             Long.parseLong(rawNums.next()),
             Long.parseLong(rawNums.next())
