c8cd821b62511ea30620a7a35e3594005eca2384
josh <josh@launchbadge.com>
2019-07-09 18:33:39 -0700

    fixes claims and adds and example

---
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/AddClaim.java
@@ -0,0 +1,35 @@
+package com.hedera.hashgraph.sdk.examples.advanced;
+
+import com.hedera.hashgraph.sdk.HederaException;
+import com.hedera.hashgraph.sdk.account.AccountAddClaimTransaction;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.examples.ExampleHelper;
+
+import java.util.Random;
+
+public class AddClaim {
+    private AddClaim() { }
+
+    public static void main(String[] args) throws HederaException {
+        var client = ExampleHelper.createHederaClient();
+
+        var userId = ExampleHelper.getOperatorId();
+        var userPubKey = ExampleHelper.getOperatorKey().getPublicKey();
+
+        // the byte claim should be a 48 byte long hash
+        // using random bytes for sake of example
+        var bytesClaim = new byte[48];
+        new Random().nextBytes(bytesClaim);
+
+        var tx = new AccountAddClaimTransaction(client)
+            .addKey(userPubKey)
+            .setAccountId(userId)
+            .setHash(bytesClaim);
+
+        System.out.println("Adding claim to " + userId.toString());
+
+        var receipt = tx.executeForReceipt();
+
+        System.out.println("Succesfully added claim to " + receipt.getAccountId().toString());
+    }
+}
