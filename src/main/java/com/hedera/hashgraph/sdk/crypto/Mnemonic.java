feafdf72d071b37717c6b353e0762f301f2c66db
Austin Bonander <austin@launchbadge.com>
2019-09-12 18:03:29 -0700

    WIP implement mnemonic phrases compatible with the mobile wallets

---
+++ src/main/java/com/hedera/hashgraph/sdk/crypto/Mnemonic.java
@@ -0,0 +1,75 @@
+package com.hedera.hashgraph.sdk.crypto;
+
+import org.bouncycastle.crypto.digests.SHA512Digest;
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.bouncycastle.crypto.params.KeyParameter;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import io.github.novacrypto.bip39.MnemonicGenerator;
+import io.github.novacrypto.bip39.wordlists.English;
+
+/**
+ * BIP-39 mnemonic phrases compatible with the Android and iOS mobile wallets.
+ */
+public final class Mnemonic {
+    /**
+     * The list of words in this mnemonic.
+     */
+    public final List<CharSequence> wordList;
+
+    @Nullable
+    private String asString;
+
+    private static final MnemonicGenerator generator = new MnemonicGenerator(English.INSTANCE);
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public Mnemonic(List<CharSequence> wordList) {
+        if (wordList.size() != 24) {
+            throw new IllegalArgumentException("wordList must have length 24");
+        }
+
+        this.wordList = Collections.unmodifiableList(wordList);
+    }
+
+    public static Mnemonic generate() {
+        final byte[] entropy = new byte[64];
+        secureRandom.nextBytes(entropy);
+
+        final ArrayList<CharSequence> wordList = new ArrayList<>(24);
+
+        generator.createMnemonic(entropy, wordList::add);
+
+        return new Mnemonic(wordList);
+    }
+
+    @Override
+    public String toString() {
+        if (asString == null) {
+            asString = String.join(" ", wordList);
+        }
+
+        return asString;
+    }
+
+    public byte[] toSeed(@Nullable String passphrase) {
+        final String salt = "mnemonic" + (passphrase != null ? passphrase : "");
+
+        // BIP-39 seed generation
+        final PKCS5S2ParametersGenerator pbkdf2 = new PKCS5S2ParametersGenerator(new SHA512Digest());
+        pbkdf2.init(
+            toString().getBytes(StandardCharsets.UTF_8),
+            salt.getBytes(StandardCharsets.UTF_8),
+            2048);
+
+        final KeyParameter key = (KeyParameter) pbkdf2.generateDerivedParameters(512);
+        return key.getKey();
+    }
+}
