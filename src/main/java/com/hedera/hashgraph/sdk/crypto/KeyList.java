736a7da637ad339daa8375183f31b8a23cadf568
Austin Bonander <austin@launchbadge.com>
2019-10-16 12:55:57 -0700

    implement `KeyList`

---
+++ src/main/java/com/hedera/hashgraph/sdk/crypto/KeyList.java
@@ -0,0 +1,26 @@
+package com.hedera.hashgraph.sdk.crypto;
+
+/**
+ * Builder for a list of keys which, if set on an account, must all sign transactions created by
+ * that account.
+ *
+ * Equivalent to a threshold key with a threshold the same as the number of keys it contains.
+ */
+public class KeyList implements Key {
+    private com.hederahashgraph.api.proto.java.KeyList.Builder keyListBuilder =
+        com.hederahashgraph.api.proto.java.KeyList.newBuilder();
+
+    public KeyList() { }
+
+    public KeyList addKey(Key key) {
+        keyListBuilder.addKeys(key.toKeyProto());
+        return this;
+    }
+
+    @Override
+    public com.hederahashgraph.api.proto.java.Key toKeyProto() {
+        com.hederahashgraph.api.proto.java.Key.Builder key = com.hederahashgraph.api.proto.java.Key.newBuilder();
+        key.setKeyList(keyListBuilder.build());
+        return key.build();
+    }
+}
