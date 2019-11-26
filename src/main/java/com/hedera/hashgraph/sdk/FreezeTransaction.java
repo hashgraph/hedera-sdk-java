97abc14daa80bf3b43ffc46b06029965e67e35c2
Austin Bonander <austin@launchbadge.com>
2019-11-25 17:20:53 -0800

    implement FreezeTransaction and serialization test

---
+++ src/main/java/com/hedera/hashgraph/sdk/FreezeTransaction.java
@@ -0,0 +1,85 @@
+package com.hedera.hashgraph.sdk;
+
+import com.hederahashgraph.api.proto.java.FreezeTransactionBody;
+import com.hederahashgraph.api.proto.java.Transaction;
+import com.hederahashgraph.api.proto.java.TransactionResponse;
+import com.hederahashgraph.service.proto.java.FreezeServiceGrpc;
+
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+
+import javax.annotation.Nullable;
+
+import io.grpc.MethodDescriptor;
+
+/**
+ * Set the freezing period in which the platform will stop creating events and accepting
+ * transactions.
+ *
+ * This is used before safely shutting down the platform for maintenance.
+ * For use by platform administrators only.
+ */
+public class FreezeTransaction extends TransactionBuilder<FreezeTransaction> {
+    private FreezeTransactionBody.Builder builder = FreezeTransactionBody.newBuilder();
+
+    public FreezeTransaction(@Nullable Client client) {
+        super(client);
+        bodyBuilder.setFreeze(builder);
+    }
+
+    @Override
+    protected void doValidate() {
+        // sanity checks, the API should not allow this
+        require(checkHour(builder.getStartHour()), "startHour not in range");
+        require(checkMinute(builder.getStartMin()), "startMin not in range");
+        require(checkHour(builder.getEndHour()), "endHour not in range");
+        require(checkMinute(builder.getEndMin()), "endMin not in range");
+    }
+
+    private static boolean checkHour(int hour) {
+        return hour >= 0 && hour < 24;
+    }
+
+    private static boolean checkMinute(int minute) {
+        return minute >= 0 && minute < 60;
+    }
+
+    /**
+     * Set the start time (hour and minute) of the freeze.
+     *
+     * The time will be converted to UTC if it isn't already. Take note that both start and end
+     * times are assumed to fall on the same day in Coordinated Universal Time.
+     *
+     * @param startTime the start time of the freeze (down to the minute), before conversion to UTC.
+     * @return {@code this} for fluent usage.
+     */
+    public FreezeTransaction setStartTime(OffsetTime startTime) {
+        OffsetTime actual = startTime.withOffsetSameInstant(ZoneOffset.UTC);
+
+        builder.setStartHour(actual.getHour());
+        builder.setStartMin(actual.getMinute());
+        return this;
+    }
+
+    /**
+     * Set the end time (hour and minute) of the freeze.
+     *
+     * The time will be converted to UTC if it isn't already. Take note that both start and end
+     * times are assumed to fall on the same day in Coordinated Universal Time.
+     *
+     * @param endTime the end time of the freeze (down to the minute), before conversion to UTC.
+     * @return {@code this} for fluent usage.
+     */
+    public FreezeTransaction setEndTime(OffsetTime endTime) {
+        OffsetTime actual = endTime.withOffsetSameInstant(ZoneOffset.UTC);
+
+        builder.setEndHour(actual.getHour());
+        builder.setEndMin(actual.getMinute());
+        return this;
+    }
+
+    @Override
+    protected MethodDescriptor<Transaction, TransactionResponse> getMethod() {
+        return FreezeServiceGrpc.getFreezeMethod();
+    }
+}
---
+++ src/test/java/com/hedera/hashgraph/sdk/FreezeTransactionTest.java
@@ -0,0 +1,75 @@
+package com.hedera.hashgraph.sdk;
+
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+
+class FreezeTransactionTest {
+
+    @Test
+    @DisplayName("correct transactions should build")
+    void correctTxn() {
+        final Instant now = Instant.ofEpochSecond(1554158542);
+        final TransactionId txnId = new TransactionId(new AccountId(2), now);
+        final AccountId nodeAcctId = new AccountId(3);
+
+        new FreezeTransaction(null)
+            .setTransactionId(txnId)
+            .setNodeAccountId(nodeAcctId)
+            // start and end times being 0:00 is technically correct
+            .build();
+
+            new FreezeTransaction(null)
+                .setTransactionId(txnId)
+                .setNodeAccountId(nodeAcctId)
+                .setStartTime(OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC))
+                .setEndTime(OffsetTime.of(23, 59, 0, 0, ZoneOffset.UTC))
+                .build();
+
+        new FreezeTransaction(null)
+            .setTransactionId(txnId)
+            .setNodeAccountId(nodeAcctId)
+            .setStartTime(OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC))
+            .setEndTime(OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC))
+            .build();
+
+        new FreezeTransaction(null)
+            .setTransactionId(txnId)
+            .setNodeAccountId(nodeAcctId)
+            .setStartTime(OffsetTime.of(23, 59, 0, 0, ZoneOffset.UTC))
+            .setEndTime(OffsetTime.of(23, 59, 0, 0, ZoneOffset.UTC))
+            .build();
+    }
+
+    @Test
+    @DisplayName("transaction should serialize properly")
+    void serializeTest() {
+        final Instant now = Instant.ofEpochSecond(1554158542);
+        final Ed25519PrivateKey key = Ed25519PrivateKey.fromString("302e020100300506032b6570042204203b054fade7a2b0869c6bd4a63b7017cbae7855d12acc357bea718e2c3e805962");
+        Transaction txn = new FreezeTransaction(null)
+            .setTransactionId(new TransactionId(new AccountId(2), now))
+            .setNodeAccountId(new AccountId(3))
+            .setStartTime(OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC))
+            .setEndTime(OffsetTime.of(23, 59, 0, 0, ZoneOffset.UTC))
+            .build()
+            .sign(key);
+
+        Assertions.assertEquals(
+            "sigMap {\n"
+                + "  sigPair {\n"
+                + "    pubKeyPrefix: \"\\344\\361\\300\\353L}\\315\\303\\347\\353\\021p\\263\\b\\212=\\022\\242\\227\\364\\243\\353\\342\\362\\205\\003\\375g5F\\355\\216\"\n"
+                + "    ed25519: \"?\\237\\317\\a>P\\245\\305\\201\\026\\3553\\365O[\\312\\344\\23781\\207*2/\\033H\\376R\\314\\334\\202\\347%\\033\\3722\\326w\\030\\313\\v]{\\t`\\233t\\247\\320E\\350\\214\\361\\376\\fV\\343\\246rz?\\317\\314\\b\"\n"
+                + "  }\n"
+                + "}\n"
+                + "bodyBytes: \"\\n\\f\\n\\006\\b\\316\\247\\212\\345\\005\\022\\002\\030\\002\\022\\002\\030\\003\\030\\200\\302\\327/\\\"\\002\\bx\\272\\001\\000\"\n",
+            txn.toProto().toString()
+        );
+    }
+}
