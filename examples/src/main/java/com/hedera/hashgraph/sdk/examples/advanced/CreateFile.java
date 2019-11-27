0bd577c6ad25f59070ef052282f003955e61326b
Austin Bonander <austin@launchbadge.com>
2019-11-27 12:40:31 -0800

    remove `ExampleHelper`

--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/CreateFile.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/CreateFile.java
@@ -3,20 +3,36 @@ package com.hedera.hashgraph.sdk.examples.advanced;
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.HederaException;
 import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
-import com.hedera.hashgraph.sdk.examples.ExampleHelper;
 import com.hedera.hashgraph.sdk.file.FileCreateTransaction;
 import com.hedera.hashgraph.sdk.file.FileId;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public final class CreateFile {
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId NODE_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("NODE_ID")));
+    private static final String NODE_ADDRESS = Objects.requireNonNull(Dotenv.load().get("NODE_ADDRESS"));
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final Ed25519PrivateKey OPERATOR_KEY = Ed25519PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
     private CreateFile() { }
 
     public static void main(String[] args) throws HederaException {
-        Ed25519PrivateKey operatorKey = ExampleHelper.getOperatorKey();
-        Client client = ExampleHelper.createHederaClient();
+        // To improve responsiveness, you should specify multiple nodes using the
+        // `Client(<Map<AccountId, String>>)` constructor instead
+        Client client = new Client(NODE_ID, NODE_ADDRESS);
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
         // The file is required to be a byte array,
         // you can easily use the bytes of a file instead.
@@ -26,7 +42,7 @@ public final class CreateFile {
             Instant.now()
                 .plus(Duration.ofSeconds(2592000)))
             // Use the same key as the operator to "own" this file
-            .addKey(operatorKey.getPublicKey())
+            .addKey(OPERATOR_KEY.getPublicKey())
             .setTransactionFee(100_000_000)
             .setContents(fileContents);
 
--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/DeleteFile.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/DeleteFile.java
@@ -3,8 +3,8 @@ package com.hedera.hashgraph.sdk.examples.advanced;
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.HederaException;
 import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
-import com.hedera.hashgraph.sdk.examples.ExampleHelper;
 import com.hedera.hashgraph.sdk.file.FileCreateTransaction;
 import com.hedera.hashgraph.sdk.file.FileDeleteTransaction;
 import com.hedera.hashgraph.sdk.file.FileId;
@@ -14,13 +14,29 @@ import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public final class DeleteFile {
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId NODE_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("NODE_ID")));
+    private static final String NODE_ADDRESS = Objects.requireNonNull(Dotenv.load().get("NODE_ADDRESS"));
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final Ed25519PrivateKey OPERATOR_KEY = Ed25519PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
     private DeleteFile() { }
 
     public static void main(String[] args) throws HederaException {
-        Ed25519PrivateKey operatorKey = ExampleHelper.getOperatorKey();
-        Client client = ExampleHelper.createHederaClient();
+        // To improve responsiveness, you should specify multiple nodes using the
+        // `Client(<Map<AccountId, String>>)` constructor instead
+        Client client = new Client(NODE_ID, NODE_ADDRESS);
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
         // The file is required to be a byte array,
         // you can easily use the bytes of a file instead.
@@ -30,7 +46,7 @@ public final class DeleteFile {
             Instant.now()
                 .plus(Duration.ofSeconds(2592000)))
             // Use the same key as the operator to "own" this file
-            .addKey(operatorKey.getPublicKey())
+            .addKey(OPERATOR_KEY.getPublicKey())
             .setContents(fileContents);
 
         TransactionReceipt receipt = tx.executeForReceipt();
