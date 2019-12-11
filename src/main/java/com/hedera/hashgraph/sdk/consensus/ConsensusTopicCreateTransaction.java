a5cf4d1cd92af4652beac29ebf4c081a1fb3b852
Ryan Leckey <ryan@launchbadge.com>
2019-12-11 05:12:42 -0800

    Update HCS

--- src/main/java/com/hedera/hashgraph/sdk/consensus/ConsensusTopicCreateTransaction.java
+++ src/main/java/com/hedera/hashgraph/sdk/consensus/ConsensusTopicCreateTransaction.java
@@ -1,13 +1,16 @@
 package com.hedera.hashgraph.sdk.consensus;
 
+import com.hedera.hashgraph.sdk.DurationHelper;
 import com.hedera.hashgraph.sdk.TimestampHelper;
 import com.hedera.hashgraph.sdk.TransactionBuilder;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.PublicKey;
 import com.hedera.hashgraph.proto.ConsensusCreateTopicTransactionBody;
 import com.hedera.hashgraph.proto.Transaction;
 import com.hedera.hashgraph.proto.TransactionResponse;
 import com.hedera.hashgraph.proto.ConsensusServiceGrpc;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import io.grpc.MethodDescriptor;
@@ -19,53 +22,28 @@ public class ConsensusTopicCreateTransaction extends TransactionBuilder<Consensu
         super();
     }
 
-    /**
-     * Short publicly visible memo about the topic. No guarantee of uniqueness.
-     */
     public ConsensusTopicCreateTransaction setTopicMemo(String topicMemo) {
         builder.setMemo(topicMemo);
         return this;
     }
 
-    /**
-     * Access control for {@link ConsensusTopicUpdateTransaction} and {@link ConsensusTopicDeleteTransaction} of this topic.
-     */
     public ConsensusTopicCreateTransaction setAdminKey(PublicKey key) {
         builder.setAdminKey(key.toKeyProto());
         return this;
     }
 
-    /**
-     * Access control for {@link ConsensusMessageSubmitTransaction}.
-     *
-     * If unspecified, no access control will be performed {@link ConsensusMessageSubmitTransaction} (all
-     * submissions would be allowed).
-     */
     public ConsensusTopicCreateTransaction setSubmitKey(PublicKey key) {
         builder.setSubmitKey(key.toKeyProto());
         return this;
     }
 
-    /**
-     * Effective consensus timestamp at which {@link ConsensusMessageSubmitTransaction} will begin to succeed on this topic.
-     *
-     * If unspecified, the consensus timestamp of this transaction will be the effective validStartTime.
-     */
-    public ConsensusTopicCreateTransaction setTopicValidStartTime(Instant validStartTime) {
-        builder.setValidStartTime(TimestampHelper.timestampFrom(validStartTime));
+    public ConsensusTopicCreateTransaction setAutoRenewPeriod(Duration autoRenewPeriod) {
+        builder.setAutoRenewPeriod(DurationHelper.durationFrom(autoRenewPeriod));
         return this;
     }
 
-    /**
-     * Effective consensus timestamp at (and after) which {@link ConsensusMessageSubmitTransaction} calls will no longer
-     * succeed on the topic.
-     *
-     * If unspecified, the consensus timestamp + 90 days will become the effective expirationTime.
-     *
-     * The expirationTime may be no longer than 90 days from the consensus timestamp of this transaction.
-     */
-    public ConsensusTopicCreateTransaction setTopicExpirationTime(Instant expirationTime) {
-        builder.setExpirationTime(TimestampHelper.timestampFrom(expirationTime));
+    public ConsensusTopicCreateTransaction setAutoRenewAccountId(AccountId autoRenewAccountId) {
+        builder.setAutoRenewAccount(autoRenewAccountId.toProto());
         return this;
     }
 
--- src/main/java/com/hedera/hashgraph/sdk/consensus/ConsensusTopicUpdateTransaction.java
+++ src/main/java/com/hedera/hashgraph/sdk/consensus/ConsensusTopicUpdateTransaction.java
@@ -1,14 +1,17 @@
 package com.hedera.hashgraph.sdk.consensus;
 
 import com.google.protobuf.StringValue;
+import com.hedera.hashgraph.sdk.DurationHelper;
 import com.hedera.hashgraph.sdk.TimestampHelper;
 import com.hedera.hashgraph.sdk.TransactionBuilder;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.PublicKey;
 import com.hedera.hashgraph.proto.ConsensusUpdateTopicTransactionBody;
 import com.hedera.hashgraph.proto.Transaction;
 import com.hedera.hashgraph.proto.TransactionResponse;
 import com.hedera.hashgraph.proto.ConsensusServiceGrpc;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import io.grpc.MethodDescriptor;
@@ -25,43 +28,33 @@ public class ConsensusTopicUpdateTransaction extends TransactionBuilder<Consensu
         return this;
     }
 
-    /**
-     * @see ConsensusTopicCreateTransaction#setTopicMemo(String)
-     */
     public ConsensusTopicUpdateTransaction setTopicMemo(String topicMemo) {
         builder.setMemo(StringValue.of(topicMemo));
         return this;
     }
 
-    /**
-     * @see ConsensusTopicCreateTransaction#setAdminKey(PublicKey)
-     */
     public ConsensusTopicUpdateTransaction setAdminKey(PublicKey key) {
         builder.setAdminKey(key.toKeyProto());
         return this;
     }
 
-    /**
-     * @see ConsensusTopicCreateTransaction#setSubmitKey(PublicKey)
-     */
     public ConsensusTopicUpdateTransaction setSubmitKey(PublicKey key) {
         builder.setSubmitKey(key.toKeyProto());
         return this;
     }
 
-    /**
-     * @see ConsensusTopicCreateTransaction#setTopicValidStartTime(Instant)
-     */
-    public ConsensusTopicUpdateTransaction setTopicValidStartTime(Instant validStartTime) {
-        builder.setValidStartTime(TimestampHelper.timestampFrom(validStartTime));
+    public ConsensusTopicUpdateTransaction setExpirationTime(Instant expirationTime) {
+        builder.setExpirationTime(TimestampHelper.timestampFrom(expirationTime));
         return this;
     }
 
-    /**
-     * @see ConsensusTopicCreateTransaction#setTopicExpirationTime(Instant)
-     */
-    public ConsensusTopicUpdateTransaction setTopicExpirationTime(Instant expirationTime) {
-        builder.setExpirationTime(TimestampHelper.timestampFrom(expirationTime));
+    public ConsensusTopicUpdateTransaction setAutoRenewPeriod(Duration autoRenewPeriod) {
+        builder.setAutoRenewPeriod(DurationHelper.durationFrom(autoRenewPeriod));
+        return this;
+    }
+
+    public ConsensusTopicUpdateTransaction setAutoRenewAccountId(AccountId autoRenewAccountId) {
+        builder.setAutoRenewAccount(autoRenewAccountId.toProto());
         return this;
     }
 
