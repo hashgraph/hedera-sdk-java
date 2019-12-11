36105a90d127ccbdf18fe7f446b607a34e3ddde8
Austin Bonander <austin@launchbadge.com>
2019-12-10 22:14:26 -0800

    fix ConsensusPubSub example

--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/ConsensusPubSub.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/advanced/ConsensusPubSub.java
@@ -1,24 +1,47 @@
 package com.hedera.hashgraph.sdk.examples.advanced;
 
+import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.HederaException;
 import com.hedera.hashgraph.sdk.Transaction;
+import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.consensus.ConsensusClient;
 import com.hedera.hashgraph.sdk.consensus.ConsensusTopicCreateTransaction;
 import com.hedera.hashgraph.sdk.consensus.ConsensusTopicId;
-import com.hedera.hashgraph.sdk.examples.ExampleHelper;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+
+import java.util.Objects;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public final class ConsensusPubSub {
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId NODE_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("NODE_ID")));
+    private static final String NODE_ADDRESS = Objects.requireNonNull(Dotenv.load().get("NODE_ADDRESS"));
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final Ed25519PrivateKey OPERATOR_KEY = Ed25519PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
     private ConsensusPubSub() { }
 
     public static void main(String[] args) throws InterruptedException, HederaException {
         final ConsensusClient consensusClient = new ConsensusClient("34.66.214.12:6552");
 
-        final Transaction topicTxn = new ConsensusTopicCreateTransaction(ExampleHelper.createHederaClient())
-            .build();
+        // To improve responsiveness, you should specify multiple nodes using the
+        // `Client(<Map<AccountId, String>>)` constructor instead
+        Client client = new Client(NODE_ID, NODE_ADDRESS);
 
-        topicTxn.execute();
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
-        final ConsensusTopicId topicId = topicTxn.queryReceipt().getTopicId();
+        final Transaction topicTxn = new ConsensusTopicCreateTransaction()
+            .setMaxTransactionFee(1_000_000_000)
+            .build(client);
+
+        topicTxn.execute(client);
+
+        final ConsensusTopicId topicId = topicTxn.getReceipt(client).getConsensusTopicId();
 
         consensusClient.subscribe(topicId, message -> {
             System.out.println("received topic message: " + message.getMessageString());
