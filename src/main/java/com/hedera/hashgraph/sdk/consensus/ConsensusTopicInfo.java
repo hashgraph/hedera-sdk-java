e1918f8bdfb5bb71d1ad0b607e6862c3df8f86e0
Austin Bonander <austin@launchbadge.com>
2019-12-20 13:31:48 -0800

    HCS code moved to `experiment/HCS`

---
+++ src/main/java/com/hedera/hashgraph/sdk/consensus/ConsensusTopicInfo.java
@@ -0,0 +1,70 @@
+package com.hedera.hashgraph.sdk.consensus;
+
+import com.hedera.hashgraph.proto.*;
+import com.hedera.hashgraph.sdk.DurationHelper;
+import com.hedera.hashgraph.sdk.Experimental;
+import com.hedera.hashgraph.sdk.TimestampHelper;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.crypto.PublicKey;
+
+import javax.annotation.Nullable;
+import java.time.Duration;
+import java.time.Instant;
+
+public class ConsensusTopicInfo {
+    public final ConsensusTopicId id;
+
+    @Nullable
+    public final String memo;
+
+    public final long sequenceNumber;
+
+    public final byte[] runningHash;
+
+    @Nullable
+    public final Instant expirationTime;
+
+    @Nullable
+    public final PublicKey adminKey;
+
+    @Nullable
+    public final PublicKey submitKey;
+
+    public final Duration autoRenewPeriod;
+
+    @Nullable
+    public final AccountId autoRenewAccount;
+
+    public ConsensusTopicInfo(com.hedera.hashgraph.proto.ConsensusGetTopicInfoResponse response) {
+        Experimental.requireFor(ConsensusTopicInfo.class.getName());
+
+        id = new ConsensusTopicId(response.getTopicIDOrBuilder());
+
+        com.hedera.hashgraph.proto.ConsensusTopicInfo info = response.getTopicInfo();
+
+        sequenceNumber = info.getSequenceNumber();
+
+        runningHash = info.getRunningHash().toByteArray();
+
+        expirationTime = info.hasExpirationTime() ? TimestampHelper.timestampTo(info.getExpirationTime()) : null;
+
+        adminKey = info.hasAdminKey() ? PublicKey.fromProtoKey(info.getAdminKey()) : null;
+
+        submitKey = info.hasSubmitKey() ? PublicKey.fromProtoKey(info.getSubmitKey()) : null;
+
+        String memo = info.getMemo();
+        this.memo = memo.isEmpty() ? null : memo;
+
+        autoRenewPeriod = DurationHelper.durationTo(info.getAutoRenewPeriod());
+
+        autoRenewAccount = info.hasAutoRenewAccount() ? new AccountId(info.getAutoRenewAccount()) : null;
+    }
+
+    static ConsensusTopicInfo fromResponse(Response response) {
+        if (!response.hasConsensusGetTopicInfo()) {
+            throw new IllegalArgumentException("response was not `consensusGetTopicInfo`");
+        }
+
+        return new ConsensusTopicInfo(response.getConsensusGetTopicInfo());
+    }
+}
