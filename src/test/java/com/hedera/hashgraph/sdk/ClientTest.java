0b7699f3cac1dc8bb0a2d2f03411d428d363f768
Austin Bonander <austin@launchbadge.com>
2019-12-26 19:48:29 -0800

    implement `Client.fromJson()` et al

---
+++ src/test/java/com/hedera/hashgraph/sdk/ClientTest.java
@@ -0,0 +1,30 @@
+package com.hedera.hashgraph.sdk;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+class ClientTest {
+    @Test
+    @DisplayName("fromJson() functions correctly")
+    void testFromJson() {
+        // put it in a file for nicer formatting
+        InputStream clientConfig = ClientTest.class.getClassLoader()
+            .getResourceAsStream("client-config.json");
+
+        Assertions.assertNotNull(clientConfig);
+
+        Client.fromJson(new InputStreamReader(clientConfig));
+
+        // put it in a file for nicer formatting
+        InputStream clientConfigWithOperator = ClientTest.class.getClassLoader()
+            .getResourceAsStream("client-config-with-operator.json");
+
+        Assertions.assertNotNull(clientConfigWithOperator);
+
+        Client.fromJson(new InputStreamReader(clientConfigWithOperator));
+    }
+}
