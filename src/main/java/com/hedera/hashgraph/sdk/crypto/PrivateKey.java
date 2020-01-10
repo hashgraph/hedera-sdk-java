1383f3f9ab7ea9e7bd2606d4f944cfbbf345c460
Austin Bonander <austin@launchbadge.com>
2020-01-09 18:09:02 -0800

    make crypto API more generic, remove `Ed25519Signature`

--- src/main/java/com/hedera/hashgraph/sdk/crypto/PrivateKey.java
+++ src/main/java/com/hedera/hashgraph/sdk/crypto/PrivateKey.java
@@ -1,8 +1,11 @@
 package com.hedera.hashgraph.sdk.crypto;
 
+import com.hedera.hashgraph.proto.SignaturePair;
+import com.hedera.hashgraph.sdk.Internal;
+
 import java.security.SecureRandom;
 
-public abstract class PrivateKey {
+public abstract class PrivateKey<PubKey extends PublicKey> {
     /**
      * A {@link SecureRandom} instance that implementations can use by default.
      */
@@ -11,9 +14,26 @@ public abstract class PrivateKey {
     protected static final SecureRandom secureRandom = new SecureRandom();
 
     /**
+     * The public-key half of this private key.
+     */
+    public final PubKey publicKey;
+
+    protected PrivateKey(PubKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    /**
      * Get the private key as DER encoded bytes.
      *
      * @return the encoded private key.
      */
     public abstract byte[] toBytes();
+
+    @Internal
+    public abstract SignaturePair sign(byte[] message, int messageOffset, int messageLen);
+
+    @Internal
+    public SignaturePair sign(byte[] message) {
+        return sign(message, 0, message.length);
+    }
 }
