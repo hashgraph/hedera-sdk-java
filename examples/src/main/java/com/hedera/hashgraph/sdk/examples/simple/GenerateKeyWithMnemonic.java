49ae64e4060cdf81fe0c37612080d3c210382480
Ryan Leckey <ryan@launchbadge.com>
2020-01-14 15:26:01 -0800

    Add example for generating key with a mnemonic

---
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/simple/GenerateKeyWithMnemonic.java
@@ -0,0 +1,20 @@
+package com.hedera.hashgraph.sdk.examples.simple;
+
+import com.hedera.hashgraph.sdk.crypto.Mnemonic;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PublicKey;
+
+public final class GenerateKeyWithMnemonic {
+    private GenerateKeyWithMnemonic() { }
+
+    public static void main(String[] args) {
+        Mnemonic mnemonic = Mnemonic.generate();
+
+        Ed25519PrivateKey newKey = mnemonic.toPrivateKey();
+        Ed25519PublicKey newPublicKey = newKey.publicKey;
+
+        System.out.println("mnemonic = " + mnemonic);
+        System.out.println("private key = " + newKey);
+        System.out.println("public key = " + newPublicKey);
+    }
+}
