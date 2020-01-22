e5255c91a0c2545b94a2d04c59f31c0d7a454f95
Ryan Leckey <ryan@launchbadge.com>
2020-01-22 02:00:08 -0800

    Add .setContractId to AccountBalanceQuery

--- src/main/java/com/hedera/hashgraph/sdk/account/AccountBalanceQuery.java
+++ src/main/java/com/hedera/hashgraph/sdk/account/AccountBalanceQuery.java
@@ -8,6 +8,7 @@ import com.hedera.hashgraph.proto.Response;
 import com.hedera.hashgraph.sdk.Hbar;
 import com.hedera.hashgraph.sdk.QueryBuilder;
 
+import com.hedera.hashgraph.sdk.contract.ContractId;
 import io.grpc.MethodDescriptor;
 
 // `CryptoGetAccountBalanceQuery`
@@ -26,14 +27,21 @@ public final class AccountBalanceQuery extends QueryBuilder<Hbar, AccountBalance
         return builder.getHeaderBuilder();
     }
 
-    public AccountBalanceQuery setAccountId(AccountId account) {
-        builder.setAccountID(account.toProto());
+    public AccountBalanceQuery setAccountId(AccountId accountId) {
+        builder.setAccountID(accountId.toProto());
+        return this;
+    }
+
+    public AccountBalanceQuery setContractId(ContractId contractId) {
+        builder.setContractID(contractId.toProto());
         return this;
     }
 
     @Override
     protected void doValidate() {
-        require(builder.hasAccountID(), ".setAccountId() required");
+        require(builder.hasAccountID() || builder.hasContractID(),
+            ".setAccountId() or .setContractId() required");
+
         require(getHeaderBuilder().hasPayment(),
             "AccountBalanceQuery requires a payment for validation but it is not processed; "
                 + "one would have been created automatically but the given Client did not have "
