b217afb7e822622d6ef6af634de12e39c3940193
Ryan Leckey <ryan@launchbadge.com>
2020-04-04 01:52:50 -0700

    build: add ErrorProne

--- src/test/java/com/hedera/hashgraph/sdk/KeyTest.java
+++ src/test/java/com/hedera/hashgraph/sdk/KeyTest.java
@@ -2,12 +2,13 @@ package com.hedera.hashgraph.sdk;
 
 import org.junit.jupiter.api.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class KeyTest {
     @Test
     void signatureVerified() {
-        byte[] message = "Hello, World".getBytes();
+        byte[] message = "Hello, World".getBytes(UTF_8);
         PrivateKey privateKey = PrivateKey.generateEd25519();
         PublicKey publicKey = privateKey.getPublicKey();
         byte[] signature = privateKey.sign(message);
