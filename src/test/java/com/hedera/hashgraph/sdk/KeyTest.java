fd66636937ecc6307b56a18c32d839884bf410a4
Ryan Leckey <ryan@launchbadge.com>
2020-04-07 02:55:00 -0700

    test: test PublicKey (de)serialization

--- src/test/java/com/hedera/hashgraph/sdk/KeyTest.java
+++ src/test/java/com/hedera/hashgraph/sdk/KeyTest.java
@@ -1,18 +1,103 @@
 package com.hedera.hashgraph.sdk;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.google.protobuf.ByteString;
+import com.hedera.hashgraph.sdk.proto.Key;
+import com.hedera.hashgraph.sdk.proto.KeyList;
+import com.hedera.hashgraph.sdk.proto.ThresholdKey;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class KeyTest {
     @Test
+    @DisplayName("can sign and verify a message")
     void signatureVerified() {
-        byte[] message = "Hello, World".getBytes(UTF_8);
-        PrivateKey privateKey = PrivateKey.generateEd25519();
-        PublicKey publicKey = privateKey.getPublicKey();
-        byte[] signature = privateKey.sign(message);
+        var message = "Hello, World".getBytes(UTF_8);
+        var privateKey = PrivateKey.generateEd25519();
+        var publicKey = privateKey.getPublicKey();
+        var signature = privateKey.sign(message);
 
-        assertThat(publicKey.verify(message, signature)).isTrue();
+        assertTrue(publicKey.verify(message, signature));
+    }
+
+    @Test
+    @DisplayName("can convert from protobuf ED25519 key to PublicKey")
+    void fromProtoKeyEd25519() {
+        var keyBytes = Hex.decode("0011223344556677889900112233445566778899001122334455667788990011");
+        var protoKey = Key.newBuilder().setEd25519(ByteString.copyFrom(keyBytes)).build();
+
+        var cut = PublicKey.fromProtobuf(protoKey);
+
+        assertEquals(cut.getClass(), PublicKey.class);
+        assertArrayEquals(keyBytes, ((PublicKey) cut).toBytes());
+    }
+
+    @Test
+    @DisplayName("can convert from protobuf key list to PublicKey")
+    void fromProtoKeyKeyList() {
+        // given
+        var keyBytes = new byte[][]{
+            Hex.decode("0011223344556677889900112233445566778899001122334455667788990011"),
+            Hex.decode("aa11223344556677889900112233445566778899001122334455667788990011")
+        };
+
+        var protoKeyList = KeyList.newBuilder();
+
+        for (byte[] kb : keyBytes) {
+            protoKeyList.addKeys(Key.newBuilder().setEd25519(ByteString.copyFrom(kb)));
+        }
+
+        var protoKey = Key.newBuilder().setKeyList(protoKeyList).build();
+
+        // when
+        var cut = com.hedera.hashgraph.sdk.Key.fromProtobuf(protoKey);
+
+        // then
+        assertEquals(cut.getClass(), com.hedera.hashgraph.sdk.KeyList.class);
+
+        var keyList = (com.hedera.hashgraph.sdk.KeyList) cut;
+        var actual = keyList.toKeyProtobuf().getKeyList();
+
+        assertEquals(2, actual.getKeysCount());
+        assertArrayEquals(keyBytes[0], actual.getKeys(0).getEd25519().toByteArray());
+        assertArrayEquals(keyBytes[1], actual.getKeys(1).getEd25519().toByteArray());
+    }
+
+    @Test
+    @DisplayName("can convert from protobuf threshold key to PublicKey")
+    void fromProtoKeyThresholdKey() {
+        // given
+        var keyBytes = new byte[][]{
+            Hex.decode("0011223344556677889900112233445566778899001122334455667788990011"),
+            Hex.decode("aa11223344556677889900112233445566778899001122334455667788990011")
+        };
+
+        var protoKeyList = KeyList.newBuilder();
+
+        for (byte[] kb : keyBytes) {
+            protoKeyList.addKeys(Key.newBuilder().setEd25519(ByteString.copyFrom(kb)));
+        }
+
+        var protoThresholdKey = ThresholdKey.newBuilder().setThreshold(1).setKeys(protoKeyList);
+        var protoKey = Key.newBuilder().setThresholdKey(protoThresholdKey).build();
+
+        // when
+        var cut = com.hedera.hashgraph.sdk.Key.fromProtobuf(protoKey);
+
+        // then
+        assertEquals(cut.getClass(), com.hedera.hashgraph.sdk.KeyList.class);
+
+        var thresholdKey = (com.hedera.hashgraph.sdk.KeyList) cut;
+        var actual = thresholdKey.toKeyProtobuf().getThresholdKey();
+
+        assertEquals(1, actual.getThreshold());
+        assertEquals(2, actual.getKeys().getKeysCount());
+        assertArrayEquals(keyBytes[0], actual.getKeys().getKeys(0).getEd25519().toByteArray());
+        assertArrayEquals(keyBytes[1], actual.getKeys().getKeys(1).getEd25519().toByteArray());
     }
 }
