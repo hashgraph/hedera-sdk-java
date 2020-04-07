7c0af1ebd0fa617b81fd7eb4ba5f0cdc8392f722
Ryan Leckey <ryan@launchbadge.com>
2020-04-06 17:13:34 -0700

    feat: MessageSubmitTransaction

--- src/main/java/com/hedera/hashgraph/sdk/MessageSubmitTransaction.java
+++ src/main/java/com/hedera/hashgraph/sdk/MessageSubmitTransaction.java
@@ -1,5 +1,6 @@
 package com.hedera.hashgraph.sdk;
 
+import com.google.protobuf.ByteString;
 import com.hedera.hashgraph.sdk.proto.ConsensusSubmitMessageTransactionBody;
 import com.hedera.hashgraph.sdk.proto.TransactionBody;
 
@@ -8,6 +9,36 @@ public final class MessageSubmitTransaction extends TransactionBuilder<MessageSu
 
     public MessageSubmitTransaction() {
         builder = ConsensusSubmitMessageTransactionBody.newBuilder();
+    }
+
+    /**
+     * Set the topic ID to submit the message to.
+     *
+     * @return {@code this}.
+     */
+    public MessageSubmitTransaction setTopicId(TopicId topicId) {
+        builder.setTopicID(topicId.toProtobuf());
+        return this;
+    }
+
+    /**
+     * Set the message to submit, as a UTF-8 string.
+     *
+     * @return {@code this}.
+     */
+    public MessageSubmitTransaction setMessage(String message) {
+        builder.setMessage(ByteString.copyFromUtf8(message));
+        return this;
+    }
+
+    /**
+     * Set the message to submit, as a byte array.
+     *
+     * @return {@code this}.
+     */
+    public MessageSubmitTransaction setMessage(byte[] message) {
+        builder.setMessage(ByteString.copyFrom(message));
+        return this;
     }
 
     @Override
