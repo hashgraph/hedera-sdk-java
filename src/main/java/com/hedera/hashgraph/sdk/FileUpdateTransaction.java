ffbfd3fe205c00abacf5163ab0198cf444656ac5
josh <josh@launchbadge.com>
2020-04-07 17:24:46 -0700

    update transaction

--- src/main/java/com/hedera/hashgraph/sdk/FileUpdateTransaction.java
+++ src/main/java/com/hedera/hashgraph/sdk/FileUpdateTransaction.java
@@ -1,5 +1,6 @@
 package com.hedera.hashgraph.sdk;
 
+import com.google.protobuf.ByteString;
 import com.hedera.hashgraph.sdk.proto.FileUpdateTransactionBody;
 import com.hedera.hashgraph.sdk.proto.TransactionBody;
 import org.threeten.bp.Instant;
@@ -41,5 +42,56 @@ public final class FileUpdateTransaction extends TransactionBuilder<FileUpdateTr
         return this;
     }
 
+    /**
+     * If set, replace contents of the file identified by {@link #setFileId(FileId)}
+     * with the given bytes.
+     * <p>
+     * If the contents of the file are longer than the given byte array, then the file will
+     * be truncated.
+     * <p>
+     * Note that total size for a given transaction is limited to 6KiB (as of March 2020) by the
+     * network; if you exceed this you may receive a {@link Status#TransactionOversize}.
+     * <p>
+     * In this case, you will need to break the data into chunks of less than ~6KiB and execute this
+     * transaction with the first chunk and then use {@link FileAppendTransaction} with
+     * {@link FileAppendTransaction#setContents(byte[])} for the remaining chunks.
+     *
+     * @param bytes the bytes to replace the contents of the file with.
+     * @return {@code this} for fluent API usage.
+     * @see #setContents(String) for an overload which takes a {@link String}.
+     * @see FileAppendTransaction if you merely want to add data to a file's existing contents.
+     */
+    public FileUpdateTransaction setContents(byte[] bytes) {
+        builder.setContents(ByteString.copyFrom(bytes));
 
+        return this;
+    }
+
+    /**
+     * If set, encode the given {@link String} as UTF-8 and replace the contents of the file
+     * identified by {@link #setFileId(FileId)}.
+     * <p>
+     * If the contents of the file are longer than the UTF-8 encoding of the given string, then the
+     * file will be truncated.
+     * <p>
+     * The string can later be recovered from {@link FileContentsQuery#execute(Client)}
+     * via {@link String#String(byte[], java.nio.charset.Charset)} using
+     * {@link java.nio.charset.StandardCharsets#UTF_8}.
+     * <p>
+     * Note that total size for a given transaction is limited to 6KiB (as of March 2020) by the
+     * network; if you exceed this you may receive a  {@link Status#TransactionOversize}.
+     * <p>
+     * In this case, you will need to break the data into chunks of less than ~6KiB and execute this
+     * transaction with the first chunk and then use {@link FileAppendTransaction} with
+     * {@link FileAppendTransaction#setContents(String)} for the remaining chunks.
+     *
+     * @param text the string to replace the contents of the file with.
+     * @return {@code this} for fluent API usage.
+     * @see #setContents(byte[]) for replacing the contents with arbitrary data.
+     * @see FileAppendTransaction if you merely want to add data to a file's existing contents.
+     */
+    public FileUpdateTransaction setContents(String text) {
+        builder.setContents(ByteString.copyFromUtf8(text));
+        return this;
+    }
 }
