ca02cf975e177b1904123f60c2a0ace815c65e8c
tim <tim@launchbadge.com>
2020-04-13 16:35:39 -0700

    add ConsensusPubSub and CreateAccount Examples, small gradle changes, remove unused var and import

---
+++ examples/src/main/java/ConsensusPubSubExample.java
@@ -0,0 +1,71 @@
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.HederaPreCheckStatusException;
+import com.hedera.hashgraph.sdk.TransactionId;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.MessageSubmitTransaction;
+import com.hedera.hashgraph.sdk.TopicCreateTransaction;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.TopicId;
+
+import com.hedera.hashgraph.sdk.MirrorClient;
+import com.hedera.hashgraph.sdk.MirrorTopicQuery;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+class ConsensusPubSubExample {
+    private ConsensusPubSubExample() {
+    }
+
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
+    @SuppressWarnings("FieldMissingNullable")
+    private static final String MIRROR_NODE_ADDRESS = Objects.requireNonNull(Dotenv.load().get("MIRROR_NODE_ADDRESS"));
+
+    @SuppressWarnings("NullableDereference")
+    public static void main(String[] args) throws TimeoutException, InterruptedException, HederaPreCheckStatusException {
+        MirrorClient mirrorClient = new MirrorClient(MIRROR_NODE_ADDRESS);
+
+        // `Client.forMainnet()` is provided for connecting to Hedera mainnet
+        Client client = Client.forTestnet();
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
+
+        TransactionId transactionId = new TopicCreateTransaction()
+            .execute(client);
+
+        TransactionReceipt transactionReceipt = transactionId.getReceipt(client);
+
+        TopicId topicId =  Objects.requireNonNull(transactionReceipt.topicId);
+
+        System.out.println("New topic created: " + topicId);
+
+        new MirrorTopicQuery()
+            .setTopicId(topicId)
+            .subscribe(mirrorClient, resp -> {
+                    String messageAsString = new String(resp.message, StandardCharsets.UTF_8);
+
+                    System.out.println(resp.consensusTimestamp + " received topic message: " + messageAsString);
+                },
+                // On gRPC error, print the stack trace
+                Throwable::printStackTrace);
+
+        // keep the main thread from exiting because the listeners run on daemon threads
+        // noinspection InfiniteLoopStatement
+        for (int i = 0; ; i++) {
+            new MessageSubmitTransaction()
+                .setTopicId(topicId)
+                .setMessage("hello, HCS! " + i)
+                .execute(client)
+                .getReceipt(client);
+
+            Thread.sleep(2500);
+        }
+    }
+}
---
+++ examples/src/main/java/CreateAccountExample.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.PublicKey;
+import com.hedera.hashgraph.sdk.HederaPreCheckStatusException;
+import com.hedera.hashgraph.sdk.TransactionId;
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public final class CreateAccountExample {
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
+    private CreateAccountExample() { }
+
+    public static void main(String[] args) throws TimeoutException, HederaPreCheckStatusException {
+        // Generate a Ed25519 private, public key pair
+        PrivateKey newKey = PrivateKey.generate();
+        PublicKey newPublicKey = newKey.getPublicKey();
+
+        System.out.println("private key = " + newKey);
+        System.out.println("public key = " + newPublicKey);
+
+        // `Client.forMainnet()` is provided for connecting to Hedera mainnet
+        try (Client client = Client.forTestnet()) {
+
+            // Defaults the operator account ID and key such that all generated transactions will be paid for
+            // by this account and be signed by this key
+            client.setOperator(OPERATOR_ID, OPERATOR_KEY);
+
+            TransactionId txId = new AccountCreateTransaction()
+                // The only _required_ property here is `key`
+                .setKey(newPublicKey)
+                .setInitialBalance(Hbar.fromTinybar(1000))
+                .execute(client);
+
+            // This will wait for the receipt to become available
+            TransactionReceipt receipt = txId.getReceipt(client);
+
+            AccountId newAccountId = receipt.accountId;
+
+            System.out.println("account = " + newAccountId);
+        }
+    }
+}
