b64b09943bf33809482963f0214839cf7bbbabc4
tim <tim@launchbadge.com>
2020-04-14 13:20:25 -0700

    add CreateFileExample

---
+++ examples/src/main/java/CreateFileExample.java
@@ -0,0 +1,43 @@
+import com.hedera.hashgraph.sdk.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public final class CreateFileExample {
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+
+    private CreateFileExample() { }
+
+    public static void main(String[] args) throws TimeoutException, HederaPreCheckStatusException {
+        // `Client.forMainnet()` is provided for connecting to Hedera mainnet
+        Client client = Client.forTestnet();
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
+
+        // The file is required to be a byte array,
+        // you can easily use the bytes of a file instead.
+        byte[] fileContents = "Hedera hashgraph is great!".getBytes(StandardCharsets.UTF_8);
+
+        TransactionId txId = new FileCreateTransaction()
+            // Use the same key as the operator to "own" this file
+            .setKeys(OPERATOR_KEY.getPublicKey())
+            .setContents(fileContents)
+            // The default max fee of 1 HBAR is not enough to make a file ( starts around 1.1 HBAR )
+            .setMaxTransactionFee(new Hbar(2)) // 2 HBAR
+            .execute(client);
+
+        TransactionReceipt receipt = txId.getReceipt(client);
+        FileId newFileId = receipt.getFileId();
+
+        System.out.println("file: " + newFileId);
+    }
+}
