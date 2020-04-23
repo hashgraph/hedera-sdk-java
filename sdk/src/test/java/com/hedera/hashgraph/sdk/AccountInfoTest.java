c1500915d4a870929af5687e1eb61e7f8c7a0d3d
tim <tim@launchbadge.com>
2020-04-22 23:27:09 -0700

    test: add ClientTest, resources for ClientTest, and AccountInfoTest

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/AccountInfoTest.java
@@ -0,0 +1,24 @@
+package com.hedera.hashgraph.sdk;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AccountInfoTest {
+    @Test
+    @DisplayName("using toBytes and fromBytes will produce the correct Id")
+    void keyGenerates() throws InvalidProtocolBufferException {
+        AccountId id = AccountId.fromString("0.0.5005");
+
+        assertNotNull(id);
+        assertNotNull(id.toBytes());
+
+        byte[] idBytes = id.toBytes();
+        AccountId newId = AccountId.fromBytes(idBytes);
+
+        assertEquals(id, newId);
+    }
+}
