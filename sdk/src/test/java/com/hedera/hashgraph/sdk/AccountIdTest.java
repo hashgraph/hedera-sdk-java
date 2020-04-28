46b3f4c037b81e432d58eec1c537c2ff7595e3c6
tim <tim@launchbadge.com>
2020-04-28 14:11:59 -0700

    test: add json-snapshot dep and add AccountId snapshot test

--- sdk/src/test/java/com/hedera/hashgraph/sdk/AccountIdTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/AccountIdTest.java
@@ -1,24 +1,50 @@
 package com.hedera.hashgraph.sdk;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static io.github.jsonSnapshot.SnapshotMatcher.expect;
+import static io.github.jsonSnapshot.SnapshotMatcher.start;
+import static io.github.jsonSnapshot.SnapshotMatcher.validateSnapshots;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountIdTest {
+    @BeforeAll
+    public static void beforeAll() {
+        start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        validateSnapshots();
+    }
+
+    AccountId setId() {
+        return AccountId.fromString("0.0.5005");
+    }
+
+    @Test
+    @DisplayName("creates account id from string correctly")
+    void fromString() {
+        AccountId id = setId();
+        expect(id).toMatchSnapshot();
+    }
+
     @Test
     @DisplayName("using toBytes and fromBytes will produce the correct Id")
     void toFromBytes() throws InvalidProtocolBufferException {
-        AccountId id = AccountId.fromString("0.0.5005");
+        byte[] idBytes = setId().toBytes();
+        AccountId id = AccountId.fromBytes(idBytes);
 
-        assertNotNull(id);
-        assertNotNull(id.toBytes());
+        expect(id).toMatchSnapshot();
+    }
 
-        byte[] idBytes = id.toBytes();
-        AccountId newId = AccountId.fromBytes(idBytes);
-
-        assertEquals(id, newId);
+    @Test
+    @DisplayName("incorrect account id from string should fail")
+    void badAccountId() {
+        expect(assertThrows(IllegalArgumentException.class, () -> AccountId.fromString("a.0.5005")).getMessage()).toMatchSnapshot();
     }
 }
