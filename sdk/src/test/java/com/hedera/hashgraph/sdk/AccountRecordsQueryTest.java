b31db0dea5236c02d4efa30ce4007df3cc0ab554
tim <tim@launchbadge.com>
2020-04-29 14:43:43 -0700

    test: add AccountRecordsQueryTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/AccountRecordsQueryTest.java
@@ -0,0 +1,27 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AccountRecordsQueryTest {
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new AccountRecordsQuery()
+            .setAccountId(AccountId.fromString("0.0.5005"))
+            .setMaxQueryPayment(Hbar.fromTinybar(100_000))
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
