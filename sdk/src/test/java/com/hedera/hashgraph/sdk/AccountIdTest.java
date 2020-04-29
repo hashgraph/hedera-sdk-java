63f415917fa66f7d402d2b90c11dba6d6c47c912
tim <tim@launchbadge.com>
2020-04-29 00:41:15 -0700

    test: revise AccountId test

--- sdk/src/test/java/com/hedera/hashgraph/sdk/AccountIdTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/AccountIdTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import static io.github.jsonSnapshot.SnapshotMatcher.expect;
 import static io.github.jsonSnapshot.SnapshotMatcher.start;
 import static io.github.jsonSnapshot.SnapshotMatcher.validateSnapshots;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountIdTest {
     @BeforeAll
@@ -27,24 +26,8 @@ class AccountIdTest {
     }
 
     @Test
-    @DisplayName("creates account id from string correctly")
-    void fromString() {
-        AccountId id = setId();
-        expect(id).toMatchSnapshot();
-    }
-
-    @Test
-    @DisplayName("using toBytes and fromBytes will produce the correct Id")
-    void toFromBytes() throws InvalidProtocolBufferException {
-        byte[] idBytes = setId().toBytes();
-        AccountId id = AccountId.fromBytes(idBytes);
-
-        expect(id).toMatchSnapshot();
-    }
-
-    @Test
-    @DisplayName("incorrect account id from string should fail")
-    void badAccountId() {
-        expect(assertThrows(IllegalArgumentException.class, () -> AccountId.fromString("a.0.5005")).getMessage()).toMatchSnapshot();
+    @DisplayName("object to be sent matches snapshot")
+    void matchesSnap(){
+        expect(setId().toProtobuf()).toMatchSnapshot();
     }
 }
