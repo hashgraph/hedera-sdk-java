131084636ed137776742718a2b39474b45fb8d06
tim <tim@launchbadge.com>
2020-04-29 14:29:44 -0700

    test: add AccountInfo test and snapshot

--- sdk/src/test/java/com/hedera/hashgraph/sdk/AccountInfoTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/AccountInfoTest.java
@@ -1,21 +1,29 @@
 package com.hedera.hashgraph.sdk;
 
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.hashgraph.sdk.proto.CryptoGetInfoResponse;
 import com.hedera.hashgraph.sdk.proto.Response;
-import org.junit.jupiter.api.DisplayName;
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AccountInfoTest {
     private static final PrivateKey privateKey = PrivateKey.fromString(
         "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
 
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
     @Test
-    @DisplayName("using toBytes and fromBytes will produce the correct response")
-    void toFromBytes() throws InvalidProtocolBufferException {
+    void fromProtobuf() {
         Response response = Response.newBuilder()
             .setCryptoGetInfo(
                 CryptoGetInfoResponse.newBuilder()
@@ -24,14 +32,6 @@ public class AccountInfoTest {
             )
             .build();
 
-        AccountInfo info = AccountInfo.fromProtobuf(response.getCryptoGetInfo().getAccountInfo());
-
-        assertNotNull(info);
-        assertNotNull(info.toBytes());
-
-        byte[] infoBytes = info.toBytes();
-        AccountInfo newInfo = AccountInfo.fromBytes(infoBytes);
-
-        assertEquals(info.toString(), newInfo.toString());
+        SnapshotMatcher.expect(AccountInfo.fromProtobuf(response.getCryptoGetInfo().getAccountInfo())).toMatchSnapshot();
     }
 }
