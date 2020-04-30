ffc4d34d2ecead88a6f81dd86668c55d1df5408a
tim <tim@launchbadge.com>
2020-04-30 11:43:47 -0700

    test: add ContractCreateTransactionTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/ContractCreateTransactionTest.java
@@ -0,0 +1,44 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Duration;
+import org.threeten.bp.Instant;
+
+public class ContractCreateTransactionTest {
+    private static final PrivateKey unusedPrivateKey = PrivateKey.fromString(
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
+
+    final Instant validStart = Instant.ofEpochSecond(1554158542);
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new ContractCreateTransaction()
+            .setNodeAccountId(AccountId.fromString("0.0.5005"))
+            .setTransactionId(new TransactionId(AccountId.fromString("0.0.5006"), validStart))
+            .setBytecodeFileId(FileId.fromString("0.0.3003"))
+            .setAdminKey(unusedPrivateKey)
+            .setGas(0)
+            .setInitialBalance(Hbar.fromTinybar(1000))
+            .setProxyAccountId(AccountId.fromString("0.0.1001"))
+            .setAutoRenewPeriod(Duration.ofHours(10))
+            .setConstructorParameters(new byte[]{10, 11, 12, 13, 25})
+            .setMaxTransactionFee(Hbar.fromTinybar(100_000))
+            .build(Client.forTestnet())
+            .sign(unusedPrivateKey)
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
