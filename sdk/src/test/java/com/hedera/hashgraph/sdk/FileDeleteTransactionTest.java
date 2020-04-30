2861936a21b5bbf49e47896d1cc5885dcbc412af
tim <tim@launchbadge.com>
2020-04-30 14:51:10 -0700

    test: add FileDeleteTransactionTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/FileDeleteTransactionTest.java
@@ -0,0 +1,37 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Instant;
+
+public class FileDeleteTransactionTest {
+    private static final PrivateKey unusedPrivateKey = PrivateKey.fromString(
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
+
+    final Instant validStart = Instant.ofEpochSecond(1554158542);
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new FileDeleteTransaction()
+            .setNodeAccountId(AccountId.fromString("0.0.5005"))
+            .setTransactionId(new TransactionId(AccountId.fromString("0.0.5006"), validStart))
+            .setFileId(FileId.fromString("0.0.6006"))
+            .setMaxTransactionFee(Hbar.fromTinybar(100_000))
+            .build(Client.forTestnet())
+            .sign(unusedPrivateKey)
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
