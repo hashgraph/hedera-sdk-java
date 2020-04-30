68a9ba5b2d4a39eaed6b7863433869a60b295a28
tim <tim@launchbadge.com>
2020-04-30 12:54:51 -0700

    refactor: remove unused variables in ContractInfoQueryTest

--- sdk/src/test/java/com/hedera/hashgraph/sdk/ContractInfoQueryTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/ContractInfoQueryTest.java
@@ -4,14 +4,8 @@ import io.github.jsonSnapshot.SnapshotMatcher;
 import org.junit.AfterClass;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.threeten.bp.Instant;
 
 public class ContractInfoQueryTest {
-    private static final PrivateKey unusedPrivateKey = PrivateKey.fromString(
-        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
-
-    final Instant validStart = Instant.ofEpochSecond(1554158542);
-
     @BeforeAll
     public static void beforeAll() {
         SnapshotMatcher.start();
