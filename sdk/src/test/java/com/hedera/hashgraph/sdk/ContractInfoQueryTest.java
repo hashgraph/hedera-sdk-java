bb8d202b75d521eddbe711fabee773ea8cc9d317
tim <tim@launchbadge.com>
2020-04-30 12:44:09 -0700

    test: add ContractInfoQueryTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/ContractInfoQueryTest.java
@@ -0,0 +1,32 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Instant;
+
+public class ContractInfoQueryTest {
+    private static final PrivateKey unusedPrivateKey = PrivateKey.fromString(
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
+
+    final Instant validStart = Instant.ofEpochSecond(1554158542);
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new ContractInfoQuery()
+            .setContractId(ContractId.fromString("0.0.5005"))
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
