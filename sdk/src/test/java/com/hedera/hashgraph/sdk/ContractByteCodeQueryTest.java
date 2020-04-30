51e13dac5d788b1dc04867d316e125d99f1ec2a0
tim <tim@launchbadge.com>
2020-04-30 11:20:13 -0700

    test: add ContractByteCodeQueryTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/ContractByteCodeQueryTest.java
@@ -0,0 +1,26 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ContractByteCodeQueryTest {
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new ContractByteCodeQuery()
+            .setContractId(ContractId.fromString("0.0.5005"))
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
