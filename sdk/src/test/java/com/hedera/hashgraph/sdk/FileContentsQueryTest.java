767bcd7e3ff8f229d4dd9244105d4d87c708cef8
tim <tim@launchbadge.com>
2020-04-30 14:15:38 -0700

    test: add FileContentsQueryTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/FileContentsQueryTest.java
@@ -0,0 +1,26 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class FileContentsQueryTest {
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void shouldSerialize() {
+        SnapshotMatcher.expect(new FileContentsQuery()
+            .setFileId(FileId.fromString("0.0.5005"))
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
