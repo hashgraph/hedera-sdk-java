8d1c909ca868abfd0fa9b710e1171ec5d78e6663
tim <tim@launchbadge.com>
2020-05-01 01:14:49 -0700

    test: add TopicUpdateTransactionTest and snapshot

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/TopicUpdateTransactionTest.java
@@ -0,0 +1,57 @@
+package com.hedera.hashgraph.sdk;
+
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Duration;
+import org.threeten.bp.Instant;
+
+public class TopicUpdateTransactionTest {
+    private static final PrivateKey unusedPrivateKey = PrivateKey.fromString(
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
+
+    final Instant validStart = Instant.ofEpochSecond(1554158542);
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void clearShouldSerialize() {
+        SnapshotMatcher.expect(new TopicUpdateTransaction()
+            .setNodeAccountId(AccountId.fromString("0.0.5005"))
+            .setTransactionId(new TransactionId(AccountId.fromString("0.0.5006"), validStart))
+            .setTopicId(TopicId.fromString("0.0.5007"))
+            .clearAdminKey()
+            .clearAutoRenewAccountId(AccountId.fromString("0.0.5008"))
+            .clearSubmitKey()
+            .clearTopicMemo()
+            .build(Client.forTestnet())
+            .sign(unusedPrivateKey)
+            .toString()
+        ).toMatchSnapshot();
+    }
+
+    @Test
+    void setShouldSerialize() {
+        SnapshotMatcher.expect(new TopicUpdateTransaction()
+            .setNodeAccountId(AccountId.fromString("0.0.5005"))
+            .setTransactionId(new TransactionId(AccountId.fromString("0.0.5006"), validStart))
+            .setTopicId(TopicId.fromString("0.0.5007"))
+            .setAdminKey(unusedPrivateKey)
+            .setAutoRenewAccountId(AccountId.fromString("0.0.5009"))
+            .setAutoRenewPeriod(Duration.ofHours(24))
+            .setSubmitKey(unusedPrivateKey)
+            .build(Client.forTestnet())
+            .sign(unusedPrivateKey)
+            .toString()
+        ).toMatchSnapshot();
+    }
+}
