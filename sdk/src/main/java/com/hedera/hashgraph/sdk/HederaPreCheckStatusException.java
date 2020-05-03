9f1706ed9853ae3103ee36f821e20e4fa4f6d5bc
Ryan Leckey <ryan@launchbadge.com>
2020-05-03 14:19:37 -0700

    docs: HederaPreCheckStatusException

--- sdk/src/main/java/com/hedera/hashgraph/sdk/HederaPreCheckStatusException.java
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/HederaPreCheckStatusException.java
@@ -2,13 +2,21 @@ package com.hedera.hashgraph.sdk;
 
 import javax.annotation.Nullable;
 
+/**
+ * Signals that a transaction has failed the pre-check.
+ * <p>
+ * Before a node submits a transaction to the rest of the network,
+ * it attempts some cheap assertions. This process is called the "pre-check".
+ */
 public final class HederaPreCheckStatusException extends Exception {
-    /** The status of the failing transaction */
+    /**
+     * The status of the failing transaction
+     */
     public final Status status;
 
     /**
      * The ID of the transaction that failed.
-     *
+     * <p>
      * This can be `null` if a query fails pre-check without an
      * associated payment transaction.
      */
