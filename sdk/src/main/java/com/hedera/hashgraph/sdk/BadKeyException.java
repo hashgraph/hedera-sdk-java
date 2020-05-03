995c4c4f33b54c655cee278f33f42cc18457482d
Ryan Leckey <ryan@launchbadge.com>
2020-05-03 14:22:00 -0700

    docs: BadKeyException

--- sdk/src/main/java/com/hedera/hashgraph/sdk/BadKeyException.java
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/BadKeyException.java
@@ -1,5 +1,11 @@
 package com.hedera.hashgraph.sdk;
 
+/**
+ * Signals that a key could not be realized from the given input.
+ * <p>
+ * This exception can be raised by any of the {@code from} methods
+ * on {@link PrivateKey} or {@link PublicKey}.
+ */
 public final class BadKeyException extends IllegalArgumentException {
     BadKeyException(String message) {
         super(message);
