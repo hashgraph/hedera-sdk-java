c12bd83530b4b063b2e584f547c9e92192609afb
tim <tim@launchbadge.com>
2020-06-23 14:32:41 -0700

    test: Add replaceNodes test

--- src/test/java/com/hedera/hashgraph/sdk/ClientTest.java
+++ src/test/java/com/hedera/hashgraph/sdk/ClientTest.java
@@ -1,11 +1,14 @@
 package com.hedera.hashgraph.sdk;
 
+import com.hedera.hashgraph.sdk.account.AccountId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 class ClientTest {
     @Test
@@ -26,5 +29,24 @@ class ClientTest {
         Assertions.assertNotNull(clientConfigWithOperator);
 
         Client.fromJson(new InputStreamReader(clientConfigWithOperator));
+    }
+
+    @Test
+    @DisplayName("replaceNodes() functions correctly")
+    void testReplaceNodes() {
+        Map<AccountId, String> nodes = new HashMap<>();
+        nodes.put(new AccountId(3), "0.testnet.hedera.com:50211");
+        nodes.put(new AccountId(4), "1.testnet.hedera.com:50211");
+
+        Client client = new Client(nodes);
+
+        nodes = new HashMap<>();
+        nodes.put(new AccountId(5), "2.testnet.hedera.com:50211");
+        nodes.put(new AccountId(6), "3.testnet.hedera.com:50211");
+
+        client.replaceNodes(nodes);
+
+        Assertions.assertNotNull(client.getNodeForId(new AccountId(5)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> client.getNodeForId(new AccountId(3)));
     }
 }
