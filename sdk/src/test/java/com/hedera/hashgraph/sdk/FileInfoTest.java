7554e2b4bea159d57ccd622b4d185acc7f2709f7
Daniel Akhterov <daniel@launchbadge.com>
2020-06-29 16:17:30 -0700

    test: add tests for FileInfo

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/FileInfoTest.java
@@ -0,0 +1,59 @@
+package com.hedera.hashgraph.sdk;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hedera.hashgraph.sdk.proto.FileGetInfoResponse;
+import com.hedera.hashgraph.sdk.proto.KeyList;
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Instant;
+
+
+public class FileInfoTest {
+    private static final PrivateKey privateKey = PrivateKey.fromString(
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10");
+
+    private static final FileGetInfoResponse.FileInfo info = FileGetInfoResponse.FileInfo.newBuilder()
+        .setFileID(new FileId(1).toProtobuf())
+        .setSize(2)
+        .setExpirationTime(InstantConverter.toProtobuf(Instant.ofEpochMilli(3)))
+        .setDeleted(true)
+        .setKeys(KeyList.newBuilder()
+            .addKeys(privateKey.getPublicKey().toKeyProtobuf()))
+        .build();
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void fromProtobuf() throws InvalidProtocolBufferException {
+        SnapshotMatcher.expect(FileInfo.fromProtobuf(info).toString())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void toProtobuf() throws InvalidProtocolBufferException {
+        SnapshotMatcher.expect(FileInfo.fromProtobuf(info).toProtobuf())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void fromBytes() throws InvalidProtocolBufferException {
+        SnapshotMatcher.expect(FileInfo.fromBytes(info.toByteArray()).toString())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void toBytes() {
+        SnapshotMatcher.expect(FileInfo.fromProtobuf(info).toBytes())
+            .toMatchSnapshot();
+    }
+}
