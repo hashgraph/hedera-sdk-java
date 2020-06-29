c1d3a56dfe1040ac1f6a2968f988cb90a86b8de0
Daniel Akhterov <daniel@launchbadge.com>
2020-06-29 14:58:41 -0700

    test: add tests for ContractInfo

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/ContractInfoTest.java
@@ -0,0 +1,59 @@
+package com.hedera.hashgraph.sdk;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hedera.hashgraph.sdk.proto.ContractGetInfoResponse;
+import io.github.jsonSnapshot.SnapshotMatcher;
+import org.junit.AfterClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.threeten.bp.Duration;
+import org.threeten.bp.Instant;
+
+
+public class ContractInfoTest {
+    private final ContractGetInfoResponse.ContractInfo info =
+        ContractGetInfoResponse.ContractInfo.newBuilder()
+            .setContractID(new ContractId(1).toProtobuf())
+            .setAccountID(new AccountId(2).toProtobuf())
+            .setContractAccountID("3")
+            .setExpirationTime(InstantConverter.toProtobuf(Instant.ofEpochMilli(4)))
+            .setAutoRenewPeriod(DurationConverter.toProtobuf(Duration.ofDays(5)))
+            .setStorage(6)
+            .setMemo("7")
+            .setBalance(8)
+            .build();
+
+    @BeforeAll
+    public static void beforeAll() {
+        SnapshotMatcher.start();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        SnapshotMatcher.validateSnapshots();
+    }
+
+    @Test
+    void fromProtobuf() {
+        SnapshotMatcher.expect(ContractInfo.fromProtobuf(info).toString())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void toProtobuf() {
+        SnapshotMatcher.expect(ContractInfo.fromProtobuf(info).toProtobuf())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void toBytes() {
+        SnapshotMatcher.expect(ContractInfo.fromProtobuf(info).toBytes())
+            .toMatchSnapshot();
+    }
+
+    @Test
+    void fromBytes() throws InvalidProtocolBufferException {
+        SnapshotMatcher.expect(ContractInfo.fromBytes(info.toByteArray()).toBytes())
+            .toMatchSnapshot();
+    }
+}
