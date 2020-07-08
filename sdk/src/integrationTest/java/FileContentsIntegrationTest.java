e99f8123b22662d7e10418c7c7697544a85e0ec8
Daniel Akhterov <daniel@launchbadge.com>
2020-07-08 16:20:29 -0700

    feat: break up file integration tests

---
+++ sdk/src/integrationTest/java/FileContentsIntegrationTest.java
@@ -0,0 +1,50 @@
+import com.google.errorprone.annotations.Var;
+import com.hedera.hashgraph.sdk.FileAppendTransaction;
+import com.hedera.hashgraph.sdk.FileContentsQuery;
+import com.hedera.hashgraph.sdk.FileCreateTransaction;
+import com.hedera.hashgraph.sdk.FileDeleteTransaction;
+import com.hedera.hashgraph.sdk.Hbar;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileContentsIntegrationTest {
+    @Test
+    void test() {
+        assertDoesNotThrow(() -> {
+            var client = IntegrationTestClientManager.getClient();
+
+            var receipt = new FileCreateTransaction()
+                .setKeys(client.getOperatorKey())
+                .setContents("[e2e::FileCreateTransaction]")
+                .setMaxTransactionFee(new Hbar(5))
+                .execute(client)
+                .getReceipt(client);
+
+            assertNotNull(receipt.fileId);
+            assertTrue(Objects.requireNonNull(receipt.fileId).num > 0);
+
+            var file = receipt.fileId;
+
+            var contents = new FileContentsQuery()
+                .setFileId(file)
+                .setQueryPayment(new Hbar(1))
+                .execute(client);
+
+            assertEquals(contents.toStringUtf8(), "[e2e::FileCreateTransaction]");
+
+            new FileDeleteTransaction()
+                .setFileId(file)
+                .setMaxTransactionFee(new Hbar(5))
+                .execute(client)
+                .getReceipt(client);
+
+            client.close();
+        });
+    }
+}
---
+++ sdk/src/integrationTest/java/FileCreateIntegrationTest.java
@@ -0,0 +1,53 @@
+import com.google.errorprone.annotations.Var;
+import com.hedera.hashgraph.sdk.FileCreateTransaction;
+import com.hedera.hashgraph.sdk.FileDeleteTransaction;
+import com.hedera.hashgraph.sdk.FileInfoQuery;
+import com.hedera.hashgraph.sdk.Hbar;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileCreateIntegrationTest {
+    @Test
+    void test() {
+        assertDoesNotThrow(() -> {
+            var client = IntegrationTestClientManager.getClient();
+
+            var receipt = new FileCreateTransaction()
+                .setKeys(client.getOperatorKey())
+                .setContents("[e2e::FileCreateTransaction]")
+                .setMaxTransactionFee(new Hbar(5))
+                .execute(client)
+                .getReceipt(client);
+
+            assertNotNull(receipt.fileId);
+            assertTrue(Objects.requireNonNull(receipt.fileId).num > 0);
+
+            var file = receipt.fileId;
+
+            @Var var info = new FileInfoQuery()
+                .setFileId(file)
+                .setQueryPayment(new Hbar(22))
+                .execute(client);
+
+            assertEquals(info.fileId, file);
+            assertEquals(info.size, 28);
+            assertFalse(info.deleted);
+            assertEquals(info.keys.get(0).toString(), client.getOperatorKey().toString());
+
+            new FileDeleteTransaction()
+                .setFileId(file)
+                .setMaxTransactionFee(new Hbar(5))
+                .execute(client)
+                .getReceipt(client);
+
+            client.close();
+        });
+    }
+}
