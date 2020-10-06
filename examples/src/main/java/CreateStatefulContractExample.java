60cd545f801a2828e62393bb05d42bbe064aa744
SimiHunjan <simi.hunjan.k@gmail.com>
2020-10-06 09:37:36 -0700

    Update CreateStatefulContractExample.java

--- examples/src/main/java/CreateStatefulContractExample.java
+++ examples/src/main/java/CreateStatefulContractExample.java
@@ -20,7 +20,7 @@ import com.hedera.hashgraph.sdk.HederaPreCheckStatusException;
 import com.hedera.hashgraph.sdk.HederaReceiptStatusException;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import com.hedera.hashgraph.sdk.TransactionReceipt;
-import com.hedera.hashgraph.sdk.TransactionResponse
+import com.hedera.hashgraph.sdk.TransactionResponse;
 
 
 import com.google.gson.Gson;
