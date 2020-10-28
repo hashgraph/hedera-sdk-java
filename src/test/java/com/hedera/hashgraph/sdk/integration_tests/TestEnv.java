e63dad3007da553529bbe1b48fde5f140e7fbfc4
Daniel Akhterov <akhterovd@gmail.com>
2020-10-28 14:39:49 -0700

    test: support `CONFIG_FILE` and `HEDERA_NETWORK` in integration test

--- src/test/java/com/hedera/hashgraph/sdk/integration_tests/TestEnv.java
+++ src/test/java/com/hedera/hashgraph/sdk/integration_tests/TestEnv.java
@@ -4,6 +4,7 @@ import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
 
+import java.io.FileNotFoundException;
 import java.util.Objects;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -17,6 +18,9 @@ public class TestEnv {
     public TestEnv() {
         final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+        String configFile = dotenv.get("CONFIG_FILE");
+        String hederaNetwork = dotenv.get("HEDERA_NETWORK");
+
         operatorId = AccountId.fromString(Objects.requireNonNull(
             dotenv.get("OPERATOR_ID"),
             "OPERATOR_ID must be set in environment or .env"));
@@ -25,6 +29,19 @@ public class TestEnv {
             dotenv.get("OPERATOR_KEY"),
             "OPERATOR_KEY must be set in environment or .env"));
 
-        client = Client.forTestnet().setOperator(operatorId, operatorKey);
+
+        Client client;
+
+        if (hederaNetwork != null && hederaNetwork.equals("previewnet")) {
+            client = Client.forPreviewnet();
+        } else {
+            try {
+                client = Client.fromFile(configFile == null ? "" : configFile);
+            } catch (FileNotFoundException e) {
+                client = Client.forTestnet();
+            }
+        }
+
+        this.client = client.setOperator(operatorId, operatorKey);
     }
 }
