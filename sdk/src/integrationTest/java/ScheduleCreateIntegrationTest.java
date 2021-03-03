0d614899b2ef2df9b6047b0400cc178c427d18c0
Daniel Akhterov <akhterovd@gmail.com>
2021-03-03 15:33:41 -0800

    feat: add scheduled topic message submit transaction test

--- sdk/src/integrationTest/java/ScheduleCreateIntegrationTest.java
+++ sdk/src/integrationTest/java/ScheduleCreateIntegrationTest.java
@@ -1,15 +1,16 @@
-import com.google.errorprone.annotations.Var;
 import com.hedera.hashgraph.sdk.*;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.threeten.bp.Instant;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Disabled
 public class ScheduleCreateIntegrationTest {
     @Test
     @DisplayName("Can create schedule")
@@ -35,7 +36,7 @@ public class ScheduleCreateIntegrationTest {
 
             var scheduleId = Objects.requireNonNull(response.getReceipt(client).scheduleId);
 
-            var info = new ScheduleInfoQuery()
+            new ScheduleInfoQuery()
                 .setScheduleId(scheduleId)
                 .setNodeAccountIds(Collections.singletonList(response.nodeId))
                 .execute(client);
@@ -116,58 +117,10 @@ public class ScheduleCreateIntegrationTest {
 
             var scheduleId = Objects.requireNonNull(response.getReceipt(client).scheduleId);
 
-            var info = new ScheduleInfoQuery()
+            new ScheduleInfoQuery()
                 .setScheduleId(scheduleId)
                 .setNodeAccountIds(Collections.singletonList(response.nodeId))
                 .execute(client);
-
-            new ScheduleDeleteTransaction()
-                .setScheduleId(scheduleId)
-                .setNodeAccountIds(Collections.singletonList(response.nodeId))
-                .execute(client)
-                .getReceipt(client);
-
-            client.close();
-        });
-    }
-
-    @Test
-    @DisplayName("Can sign schedule")
-    void canSignSchedule1() {
-        assertDoesNotThrow(() -> {
-            var client = IntegrationTestClientManager.getClient();
-            var operatorKey = Objects.requireNonNull(client.getOperatorPublicKey());
-            var operatorId = Objects.requireNonNull(client.getOperatorAccountId());
-
-            var key = PrivateKey.generate();
-
-            var transaction = new AccountCreateTransaction()
-                .setKey(key)
-                .setInitialBalance(new Hbar(10))
-                .setNodeAccountIds(Collections.singletonList(new AccountId(3)))
-                .freezeWith(client);
-
-            var tx = transaction.schedule();
-
-            var response = tx
-                .setAdminKey(operatorKey)
-                .setPayerAccountId(operatorId)
-                .execute(client);
-
-            var signature = key.signTransaction(transaction);
-
-            var scheduleId = Objects.requireNonNull(response.getReceipt(client).scheduleId);
-
-            var info = new ScheduleInfoQuery()
-                .setScheduleId(scheduleId)
-                .setNodeAccountIds(Collections.singletonList(response.nodeId))
-                .execute(client);
-
-            var signTx = new ScheduleSignTransaction()
-                .setScheduleId(scheduleId)
-                .setNodeAccountIds(Collections.singletonList(response.nodeId))
-                .execute(client)
-                .getReceipt(client);
 
             new ScheduleDeleteTransaction()
                 .setScheduleId(scheduleId)
@@ -206,7 +159,7 @@ public class ScheduleCreateIntegrationTest {
 
             var scheduleId = Objects.requireNonNull(response.getReceipt(client).scheduleId);
 
-            var signTransaction = new ScheduleSignTransaction()
+            new ScheduleSignTransaction()
                 .addScheduleSignature(key.getPublicKey(), signature)
                 .setNodeAccountIds(Collections.singletonList(response.nodeId))
                 .setScheduleId(scheduleId)
@@ -220,5 +173,86 @@ public class ScheduleCreateIntegrationTest {
 
             client.close();
         });
+    }
+
+    @Test
+    @DisplayName("Can schedule topic message")
+    void canScheduleTopicMessage() throws TimeoutException, PrecheckStatusException, ReceiptStatusException {
+        Client client = IntegrationTestClientManager.getClient();
+        var operatorKey = Objects.requireNonNull(client.getOperatorPublicKey());
+        var operatorId = Objects.requireNonNull(client.getOperatorAccountId());
+
+        // Generate 3 random keys
+        var key1 = PrivateKey.generate();
+
+        // This is the submit key
+        var key2 = PrivateKey.generate();
+
+        var key3 = PrivateKey.generate();
+
+        var keyList = new KeyList();
+
+        keyList.add(key1.getPublicKey());
+        keyList.add(key2.getPublicKey());
+        keyList.add(key3.getPublicKey());
+
+        var response = new AccountCreateTransaction()
+            .setInitialBalance(new Hbar(100))
+            .setKey(keyList)
+            .execute(client);
+
+       assertNotNull(response.getReceipt(client).accountId);
+
+        var topicId = Objects.requireNonNull(new TopicCreateTransaction()
+            .setAdminKey(operatorKey)
+            .setAutoRenewAccountId(operatorId)
+            .setTopicMemo("HCS Topic_")
+            .setSubmitKey(key2.getPublicKey())
+            .execute(client)
+            .getReceipt(client)
+            .topicId
+        );
+
+        var transaction = new TopicMessageSubmitTransaction()
+            .setNodeAccountIds(Collections.singletonList(response.nodeId))
+            .setTopicId(topicId)
+            .setMessage("scheduled hcs message".getBytes(StandardCharsets.UTF_8))
+            .freezeWith(client);
+
+        // create schedule
+        var scheduleId = Objects.requireNonNull(transaction.schedule()
+            .setAdminKey(operatorKey)
+            .setPayerAccountId(operatorId)
+            .setMemo("mirror scheduled E2E signature on create and sign_" + Instant.now())
+            .execute(client)
+            .getReceipt(client)
+            .scheduleId
+        );
+
+        // verify schedule has been created and has 1 of 2 signatures
+        var info = new ScheduleInfoQuery()
+            .setScheduleId(scheduleId)
+            .setNodeAccountIds(Collections.singletonList(response.nodeId))
+            .execute(client);
+
+        assertNotNull(info);
+        assertEquals(info.scheduleId, scheduleId);
+
+        var key2Signature = key2.signTransaction(transaction);
+
+        new ScheduleSignTransaction()
+            .setScheduleId(scheduleId)
+            .addScheduleSignature(key2.getPublicKey(), key2Signature)
+            .execute(client)
+            .getReceipt(client);
+
+        var error = assertThrows(PrecheckStatusException.class, () -> {
+            new ScheduleInfoQuery()
+                .setScheduleId(scheduleId)
+                .setNodeAccountIds(Collections.singletonList(response.nodeId))
+                .execute(client);
+        });
+
+        assertTrue(error.getMessage().contains(Status.INVALID_SCHEDULE_ID.toString()));
     }
 }
