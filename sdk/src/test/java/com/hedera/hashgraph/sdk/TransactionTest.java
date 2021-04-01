c73e1f5ef18c7ef74950b472b14590cfdcb87504
Daniel Akhterov <akhterovd@gmail.com>
2021-04-01 15:48:28 -0700

    refactor: `Transaction.fromBytes()` should support old `proto.Transaction` bytes just like in v1

---
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/TransactionTest.java
@@ -0,0 +1,19 @@
+package com.hedera.hashgraph.sdk;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionTest {
+    @Test
+    void transactionFromBytesWorksWithProtobufTransactionBytes() throws InvalidProtocolBufferException {
+        var bytes = Hex.decode("1acc010a640a2046fe5013b6f6fc796c3e65ec10d2a10d03c07188fc3de13d46caad6b8ec4dfb81a4045f1186be5746c9783f68cb71d6a71becd3ffb024906b855ac1fa3a2601273d41b58446e5d6a0aaf421c229885f9e70417353fab2ce6e9d8e7b162e9944e19020a640a20f102e75ff7dc3d72c9b7075bb246fcc54e714c59714814011e8f4b922d2a6f0a1a40f2e5f061349ab03fa21075020c75cf876d80498ae4bac767f35941b8e3c393b0e0a886ede328e44c1df7028ea1474722f2dcd493812d04db339480909076a10122500a180a0c08a1cc98830610c092d09e0312080800100018e4881d120608001000180418b293072202087872240a220a0f0a080800100018e4881d10ff83af5f0a0f0a080800100018eb881d108084af5f");
+
+        var transaction = (TransferTransaction)Transaction.fromBytes(bytes);
+
+        assertEquals(new Hbar(1).negated(), transaction.getHbarTransfers().get(new AccountId(476260)));
+        assertEquals(new Hbar(1), transaction.getHbarTransfers().get(new AccountId(476267)));
+    }
+}
