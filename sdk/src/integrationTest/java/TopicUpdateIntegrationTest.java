d749bed12ff1e5f366003ecb0a9d0c7f067514b8
Sean Tedrow <sean.tedrow@launchbadge.com>
2021-07-13 18:25:25 -0700

    fix: TopicUpdateTransaction::clearAutoRenewAccountId() should now work correctly
    Signed-off-by: Sean Tedrow <sean.tedrow@launchbadge.com>

---
+++ sdk/src/integrationTest/java/TopicUpdateIntegrationTest.java
@@ -0,0 +1,51 @@
+import com.hedera.hashgraph.sdk.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+public class TopicUpdateIntegrationTest {
+    @Test
+    @DisplayName("Can update topic")
+    void canUpdateTopic() {
+        assertDoesNotThrow(() -> {
+            var testEnv = new IntegrationTestEnv();
+
+            var response = new TopicCreateTransaction()
+                .setNodeAccountIds(testEnv.nodeAccountIds)
+                .setAdminKey(testEnv.operatorKey)
+                .setAutoRenewAccountId(testEnv.operatorId)
+                .setTopicMemo("[e2e::TopicCreateTransaction]")
+                .execute(testEnv.client);
+
+            var topicId = Objects.requireNonNull(response.getReceipt(testEnv.client).topicId);
+
+            new TopicUpdateTransaction()
+                .setNodeAccountIds(testEnv.nodeAccountIds)
+                .clearAutoRenewAccountId()
+                .setTopicMemo("hello")
+                .setTopicId(topicId)
+                .execute(testEnv.client)
+                .getReceipt(testEnv.client);
+
+            var topicInfo = new TopicInfoQuery()
+                .setTopicId(topicId)
+                .execute(testEnv.client);
+
+            assertEquals(topicInfo.topicMemo, "hello");
+            assertNull(topicInfo.autoRenewAccountId);
+
+            new TopicDeleteTransaction()
+                .setTopicId(topicId)
+                .execute(testEnv.client)
+                .getReceipt(testEnv.client);
+
+            testEnv.client.close();
+        });
+    }
+}
