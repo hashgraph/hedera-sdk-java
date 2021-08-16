706d46ade5c646aebd4159f129bff8495f3f09a8
Sean Tedrow <sean.tedrow@launchbadge.com>
2021-08-16 15:20:13 -0700

    fix: transactions now retry on RST_STREAM
    Signed-off-by: Sean Tedrow <sean.tedrow@launchbadge.com>

--- sdk/src/main/java/com/hedera/hashgraph/sdk/TopicMessageQuery.java
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/TopicMessageQuery.java
@@ -25,13 +25,10 @@ import java.util.HashMap;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.regex.Pattern;
 
 public final class TopicMessageQuery {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TopicMessageQuery.class);
-    private static final Pattern RST_STREAM = Pattern
-        .compile(".*\\brst[^0-9a-zA-Z]stream\\b.*", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     private final ConsensusTopicQuery.Builder builder;
     private Runnable completionHandler = this::onComplete;
@@ -135,9 +132,9 @@ public final class TopicMessageQuery {
             var description = statusRuntimeException.getStatus().getDescription();
 
             return (code == Status.Code.NOT_FOUND) ||
-                (code == Status.Code.UNAVAILABLE) ||
-                (code == Status.Code.RESOURCE_EXHAUSTED) ||
-                (code == Status.Code.INTERNAL && description != null && RST_STREAM.matcher(description).matches());
+                    (code == Status.Code.UNAVAILABLE) ||
+                    (code == Status.Code.RESOURCE_EXHAUSTED) ||
+                    (code == Status.Code.INTERNAL && description != null && Executable.RST_STREAM.matcher(description).matches());
         }
 
         return false;
@@ -239,7 +236,7 @@ public final class TopicMessageQuery {
                 var delay = Math.min(500 * (long) Math.pow(2, attempt), maxBackoff.toMillis());
                 var topicId = TopicId.fromProtobuf(builder.getTopicID());
                 LOGGER.warn("Error subscribing to topic {} during attempt #{}. Waiting {} ms before next attempt: {}",
-                    topicId, attempt, delay, t.getMessage());
+                        topicId, attempt, delay, t.getMessage());
                 call.cancel("unsubscribed", null);
 
                 // Cannot use `CompletableFuture<U>` here since this future is never polled
