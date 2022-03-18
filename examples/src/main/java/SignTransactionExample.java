893b3ab5878911b48474f3417d9e0f17d650744f
Ricky Rodriguez <ricky.rodriguez@launchbadge.com>
2022-03-18 13:22:02 -0700

    initial implementation SignTransactionExample
    Signed-off-by: Ricky Rodriguez <rickyrod@mac.com>

---
+++ examples/src/main/java/SignTransactionExample.java
@@ -0,0 +1,73 @@
+import com.google.errorprone.annotations.Var;
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.PrecheckStatusException;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.KeyList;
+import com.hedera.hashgraph.sdk.TransactionResponse;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.TransferTransaction;
+import com.hedera.hashgraph.sdk.ReceiptStatusException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+public class SignTransactionExample {
+
+
+    // see `.env.sample` in the repository root for how to specify these values
+    // or set environment variables with the same names
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+    // HEDERA_NETWORK defaults to testnet if not specified in dotenv
+    private static final String HEDERA_NETWORK = Dotenv.load().get("HEDERA_NETWORK", "testnet");
+
+    private SignTransactionExample() {
+    }
+
+    public static void main(String[] args) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+
+        Client client = Client.forName(HEDERA_NETWORK);
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
+
+        PrivateKey user1Key = PrivateKey.generateED25519();
+        PrivateKey user2Key = PrivateKey.generateED25519();
+
+        KeyList keylist = new KeyList();
+        keylist.add(user1Key);
+        keylist.add(user2Key);
+
+        TransactionResponse createAccountTransaction = new AccountCreateTransaction()
+            .setInitialBalance(new Hbar(2))
+            .setKey(keylist)
+            .execute(client);
+
+        @Var
+        TransactionReceipt receipt = createAccountTransaction.getReceipt(client);
+
+        System.out.println("account id = " + receipt.accountId);
+
+        TransferTransaction transferTransaction = new TransferTransaction()
+            .setNodeAccountIds(Collections.singletonList(new AccountId(3)))
+            .addHbarTransfer(receipt.accountId, Hbar.from(-1))
+            .addHbarTransfer(new AccountId(3), new Hbar(1))
+            .freezeWith(client);
+
+        transferTransaction.signWithOperator(client);
+        user1Key.signTransaction(transferTransaction);
+        user2Key.signTransaction(transferTransaction);
+
+        TransactionResponse result = transferTransaction.execute(client);
+        receipt = result.getReceipt(client);
+
+        System.out.println(receipt.status);
+
+    }
+}
