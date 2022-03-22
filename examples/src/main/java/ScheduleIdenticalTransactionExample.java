3854a69d32f0ff25e3bfe902a29013d06379d0ef
Ricky Rodriguez <ricky.rodriguez@launchbadge.com>
2022-03-22 14:06:15 -0700

    WIP java errors go does not java ScheduleCreateTransaction has frozen check go NewScheduleCreateTransaction does not
    Signed-off-by: Ricky Rodriguez <ricky.rodriguez@launchbadge.com>

--- examples/src/main/java/ScheduleIdenticalTransactionExample.java
+++ examples/src/main/java/ScheduleIdenticalTransactionExample.java
@@ -1,15 +1,25 @@
+import com.google.errorprone.annotations.Var;
 import com.hedera.hashgraph.sdk.AccountCreateTransaction;
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.KeyList;
 import com.hedera.hashgraph.sdk.PrecheckStatusException;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import com.hedera.hashgraph.sdk.PublicKey;
 import com.hedera.hashgraph.sdk.ReceiptStatusException;
+import com.hedera.hashgraph.sdk.ScheduleCreateTransaction;
+import com.hedera.hashgraph.sdk.ScheduleId;
+import com.hedera.hashgraph.sdk.ScheduleSignTransaction;
+import com.hedera.hashgraph.sdk.Status;
 import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.TransactionRecord;
+import com.hedera.hashgraph.sdk.TransactionRecordQuery;
 import com.hedera.hashgraph.sdk.TransactionResponse;
+import com.hedera.hashgraph.sdk.TransferTransaction;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
@@ -40,7 +50,8 @@ public class ScheduleIdenticalTransactionExample {
         Client[] clients = new Client[3];
         AccountId[] accounts = new AccountId[3];
 
-//        ScheduleId scheduleID = new ScheduleId();
+        @Var
+        ScheduleId scheduleID = null;
 
         // Loop to generate keys, clients, and accounts
         for (int i = 0; i < 3 ; i++) {
@@ -65,9 +76,82 @@ public class ScheduleIdenticalTransactionExample {
             accounts[i] = transactionReceipt.accountId;
 
             System.out.println("account = " + accounts[i]);
+        }   // Loop to generate keys, clients, and accounts
+
+        // A threshold key with a threshold of 2 and length of 3 requires
+        // at least 2 of the 3 keys to sign anything modifying the account
+        KeyList keyList = KeyList.withThreshold(2);
+        Collections.addAll(keyList, pubKeys);
+
+        // We are using all of these keys, so the scheduled transaction doesn't automatically go through
+        // It works perfectly fine with just one key
+        TransactionResponse createResponse = new AccountCreateTransaction()
+            // The key that must sign each transfer out of the account. If receiverSigRequired is true, then
+            // it must also sign any transfer into the account.
+            .setKey(keyList)
+            .setInitialBalance(new Hbar(10))
+            .execute(client);
+
+        // Make sure the transaction succeeded
+        TransactionReceipt receipt = createResponse.getReceipt(client);
+
+        AccountId thresholdAccount = receipt.accountId;
+        System.out.println("threshold account = " + thresholdAccount);
+
+        for (Client loopClient : clients) {
+            AccountId operatorId = loopClient.getOperatorAccountId();
+
+            // Each loopClient creates an identical transaction, sending 1 hbar to each of the created accounts,
+            // sent from the threshold Account
+            TransferTransaction tx = new TransferTransaction();
+            for (AccountId account : accounts) {
+                tx.addHbarTransfer(account, new Hbar(1));
+            }
+            tx.addHbarTransfer(thresholdAccount, new Hbar(3).negated());
+            tx.freezeWith(loopClient);
+            TransferTransaction signedTransaction = tx.signWithOperator(loopClient);
+
+            @Var
+            ScheduleCreateTransaction scheduledTx = new ScheduleCreateTransaction()
+                .setScheduledTransaction(signedTransaction);
+
+            scheduledTx = scheduledTx.setPayerAccountId(thresholdAccount);
+
+            TransactionResponse response = scheduledTx.execute(loopClient);
+
+            TransactionRecord loopReceipt = new TransactionRecordQuery()
+                .setTransactionId(response.transactionId)
+                .setNodeAccountIds(Collections.singletonList(response.nodeId))
+                .execute(loopClient);
+
+            System.out.println("operator [" + operatorId + "]: scheduleID = " + receipt.scheduleId);
+
+            // Save the schedule ID, so that it can be asserted for each loopClient submission
+            if (scheduleID == null) {
+                scheduleID = loopReceipt.receipt.scheduleId;
+            }
+
+            if (!Objects.equals(scheduleID, loopReceipt.receipt.scheduleId)) {
+                System.out.println("invalid generated schedule id, expected " + scheduleID + ", got " + loopReceipt.receipt.scheduleId);
+                return;
+            }
+
+            // If the status return by the receipt is related to already created, execute a schedule sign transaction
+            if (receipt.status == Status.IDENTICAL_SCHEDULE_ALREADY_CREATED) {
+                TransactionResponse signTransaction = new ScheduleSignTransaction()
+                    .setScheduleId(scheduleID)
+                    .setNodeAccountIds(Collections.singletonList(createResponse.nodeId))
+                    .setScheduleId(loopReceipt.receipt.scheduleId)
+                    .execute(loopClient);
+
+                signTransaction.getReceipt(loopClient);
+//                if err != nil {
+//                    if err.Error() != "exceptional receipt status: SCHEDULE_ALREADY_EXECUTED" {
+//                        println(err.Error(), ": error while getting scheduled sign with operator ", operatorId);
+//                        return;
+//                    }
+                }
+            }
         }
 
-
-
-    }
 }
