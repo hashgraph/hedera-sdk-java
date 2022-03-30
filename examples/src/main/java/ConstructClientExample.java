75f40fe440fc0e1b195a4e83c1edb201e65c9afa
Ricky Rodriguez <ricky.rodriguez@launchbadge.com>
2022-03-29 23:48:35 -0700

    added / fixed examples updated CHANGELOG.md (#937)
    Signed-off-by: Ricky Rodriguez <ricky.rodriguez@launchbadge.com>

--- examples/src/main/java/ConstructClientExample.java
+++ examples/src/main/java/ConstructClientExample.java
@@ -92,5 +92,7 @@ public class ConstructClientExample {
         mainClient.close();
         namedNetworkClient.close();
         customClient.close();
+
+        System.out.println("Success!");
     }
 }
