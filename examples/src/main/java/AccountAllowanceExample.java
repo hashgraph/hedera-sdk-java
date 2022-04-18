50cb5db29085353987dbff521fe0ec0d7cba30b5
Ricky Rodriguez <ricky.rodriguez@launchbadge.com>
2022-04-17 18:33:55 -0700

    Add License Headers (#952)
    Co-authored-by: Daniel Akhterov <daniel@launchbadge.com>

--- examples/src/main/java/AccountAllowanceExample.java
+++ examples/src/main/java/AccountAllowanceExample.java
@@ -1,4 +1,23 @@
-;import com.hedera.hashgraph.sdk.AccountAllowanceAdjustTransaction;
+/*-
+ *
+ * Hedera Java SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import com.hedera.hashgraph.sdk.AccountAllowanceAdjustTransaction;
 import com.hedera.hashgraph.sdk.AccountAllowanceApproveTransaction;
 import com.hedera.hashgraph.sdk.AccountAllowanceDeleteTransaction;
 import com.hedera.hashgraph.sdk.AccountBalanceQuery;
@@ -10,12 +29,10 @@ import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.Hbar;
 import com.hedera.hashgraph.sdk.PrecheckStatusException;
 import com.hedera.hashgraph.sdk.PrivateKey;
-import com.hedera.hashgraph.sdk.PublicKey;
 import com.hedera.hashgraph.sdk.ReceiptStatusException;
 import com.hedera.hashgraph.sdk.TransactionId;
 import com.hedera.hashgraph.sdk.TransferTransaction;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.checkerframework.checker.units.qual.A;
 
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
