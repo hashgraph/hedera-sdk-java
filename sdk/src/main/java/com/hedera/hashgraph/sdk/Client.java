efa642765ba8e1f17849b7332bf4bdf1a4d022f6
Sean Tedrow <sean.tedrow@launchbadge.com>
2022-08-16 16:55:21 -0700

    doc: update changelog
    Signed-off-by: Sean Tedrow <sean.tedrow@launchbadge.com>

--- sdk/src/main/java/com/hedera/hashgraph/sdk/Client.java
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/Client.java
@@ -301,7 +301,7 @@ public final class Client implements AutoCloseable, WithPing, WithPingAll {
                 String mirror = config.mirrorNetwork.getAsString();
                 switch (mirror) {
                     case "mainnet":
-                        client.setMirrorNetwork(Lists.of("hcs.mainnet.mirrornode.hedera.com:5600"));
+                        client.setMirrorNetwork(Lists.of("mainnet-public.mirrornode.hedera.com:5600"));
                         break;
                     case "testnet":
                         client.setMirrorNetwork(Lists.of("hcs.testnet.mirrornode.hedera.com:5600"));
