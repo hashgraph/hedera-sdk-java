354d385bc5a676b33fb67a4cc1b1ee1d093c6ec0
Sean Tedrow <sean.tedrow@launchbadge.com>
2022-09-01 19:44:23 -0700

    doc: more tweaks
    Signed-off-by: Sean Tedrow <sean.tedrow@launchbadge.com>

--- examples/src/main/java/SolidityPrecompileExample.java
+++ examples/src/main/java/SolidityPrecompileExample.java
@@ -39,75 +39,82 @@ public class SolidityPrecompileExample {
     }
 
     public static void main(String[] args) throws TimeoutException, PrecheckStatusException, ReceiptStatusException, IOException {
-        Client client = Client.forName(HEDERA_NETWORK);
+        try (Client client = Client.forName(HEDERA_NETWORK)) {
 
-        // Defaults the operator account ID and key such that all generated transactions will be paid for
-        // by this account and be signed by this key
-        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
+            // Defaults the operator account ID and key such that all generated transactions will be paid for
+            // by this account and be signed by this key
+            client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
-        // We need some new accounts for the contract to interact with in some of its steps
+            // We need some new accounts for the contract to interact with in some of its steps
 
-        PrivateKey alicePrivateKey = PrivateKey.generateED25519();
-        PublicKey alicePublicKey = alicePrivateKey.getPublicKey();
-        AccountId aliceAccountId = Objects.requireNonNull(new AccountCreateTransaction()
-            .setKey(alicePublicKey)
-            .setInitialBalance(Hbar.from(10))
-            .execute(client)
-            .getReceipt(client)
-            .accountId
-        );
+            PrivateKey alicePrivateKey = PrivateKey.generateED25519();
+            PublicKey alicePublicKey = alicePrivateKey.getPublicKey();
+            AccountId aliceAccountId = Objects.requireNonNull(new AccountCreateTransaction()
+                .setKey(alicePublicKey)
+                .setInitialBalance(Hbar.from(10))
+                .execute(client)
+                .getReceipt(client)
+                .accountId
+            );
 
-        PrivateKey bobPrivateKey = PrivateKey.generateED25519();
-        PublicKey bobPublicKey = bobPrivateKey.getPublicKey();
-        AccountId bobAccountId = Objects.requireNonNull(new AccountCreateTransaction()
-            .setKey(alicePublicKey)
-            .setInitialBalance(Hbar.from(10))
-            .execute(client)
-            .getReceipt(client)
-            .accountId
-        );
+            PrivateKey bobPrivateKey = PrivateKey.generateED25519();
+            PublicKey bobPublicKey = bobPrivateKey.getPublicKey();
+            AccountId bobAccountId = Objects.requireNonNull(new AccountCreateTransaction()
+                .setKey(alicePublicKey)
+                .setInitialBalance(Hbar.from(10))
+                .execute(client)
+                .getReceipt(client)
+                .accountId
+            );
 
-        ContractHelper contractHelper = new ContractHelper(
-            ContractHelper.getJsonResource("precompile-example/PrecompileExample.json"),
-            new ContractFunctionParameters()
-                .addAddress(OPERATOR_ID.toSolidityAddress())
-                .addAddress(aliceAccountId.toSolidityAddress())
-                .addAddress(bobAccountId.toSolidityAddress()),
-            client
-        );
+            // Instantiate ContractHelper
 
-        try {
+            ContractHelper contractHelper = new ContractHelper(
+                ContractHelper.getJsonResource("precompile-example/PrecompileExample.json"),
+                new ContractFunctionParameters()
+                    .addAddress(OPERATOR_ID.toSolidityAddress())
+                    .addAddress(aliceAccountId.toSolidityAddress())
+                    .addAddress(bobAccountId.toSolidityAddress()),
+                client
+            );
+
+            // Configure steps in ContracHelper
+
             contractHelper
                 .setResultValidatorForStep(0, contractFunctionResult -> {
                     System.out.println("getPseudoRandomSeed() returned " + Arrays.toString(contractFunctionResult.getBytes32(0)));
                     return true;
                 }).setPayableAmountForStep(1, Hbar.from(20))
-                .setParameterSupplierForStep(2, () -> {
+                .setParameterSupplierForStep(11, () -> {
                     return new ContractFunctionParameters()
                         // when contracts work with a public key, they handle the raw bytes of the public key
                         .addBytes(alicePublicKey.toBytesRaw());
-                }).setPayableAmountForStep(2, Hbar.from(40))
+                }).setPayableAmountForStep(11, Hbar.from(40))
                 // Because we're setting the adminKey for the created NFT token to Alice's key,
                 // Alice must sign the ContractExecuteTransaction.
-                .addSignerForStep(2, alicePrivateKey)
-                // step 0 tests PRNG
-                // step 1 creates a fungible token
-                // step 2 mints it
-                // step 3 associates Alice with it
-                // step 4 transfers it.
-                // step 5 approves an allowance of the fungible token with operator as the owner and alice as the spender
-                // step 6 Alice spends some of her fungible token allowance.
-                // steps 7 - 10 test misc functions on the fungible token.
-                // step 11 creates an NFT token
-                // step 12 mints it
-                // step 13 transfers it
-                // step 14 approves an NFT allowance with operator as the owner and Alice as the spender
-                // step 15 Alice spends some of her NFT allowance.
+                .addSignerForStep(11, alicePrivateKey);
+
+            // step 0 tests PRNG
+            // step 1 creates a fungible token
+            // step 2 mints it
+            // step 3 associates Alice with it
+            // step 4 transfers it.
+            // step 5 approves an allowance of the fungible token with operator as the owner and alice as the spender
+            // step 6 Alice spends some of her fungible token allowance.
+            // steps 7 - 10 test misc functions on the fungible token.
+            // step 11 creates an NFT token
+            // step 12 mints it
+            // step 13 transfers it
+            // step 14 approves an NFT allowance with operator as the owner and Alice as the spender
+            // step 15 Alice spends some of her NFT allowance.
+
+            contractHelper
+                //.executeSteps(/* from step */ 0, /* to step */ 6, client)
+                //.executeSteps(/* from step */ 10, /* to step */ 15, client);
                 .executeSteps(/* from step */ 2, /* to step */ 2, client);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        client.close();
     }
 }
