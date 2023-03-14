a35c4d7b50ff19943cb74f66bed14455c2a9571b
Nikola Genov <nikola.genov@limechain.tech>
2023-03-14 15:58:39 +0200

    TokenNftInfoQuery properties test
    Signed-off-by: Nikola Genov <nikola.genov@limechain.tech>

--- sdk/src/test/java/com/hedera/hashgraph/sdk/TokenNftInfoQueryTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/TokenNftInfoQueryTest.java
@@ -6,6 +6,8 @@ import org.junit.AfterClass;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TokenNftInfoQueryTest {
     @BeforeAll
     public static void beforeAll() {
@@ -25,5 +27,23 @@ public class TokenNftInfoQueryTest {
             .setMaxQueryPayment(Hbar.fromTinybars(100_000))
             .onMakeRequest(builder, QueryHeader.newBuilder().build());
         SnapshotMatcher.expect(builder.build().toString().replaceAll("@[A-Za-z0-9]+", "")).toMatchSnapshot();
+    }
+
+    @Test
+    void propertiesTest() {
+        var tokenId = TokenId.fromString("0.0.5005");
+        var query = new TokenNftInfoQuery()
+            .byAccountId(AccountId.fromString("0.0.123"))
+            .byTokenId(tokenId)
+            .setStart(5)
+            .setEnd(8)
+            .setNftId(tokenId.nft(101))
+            .setMaxQueryPayment(Hbar.fromTinybars(100_000));
+
+        assertThat(query.getNftId()).hasToString("0.0.5005/101");
+        assertThat(query.getTokenId()).isEqualTo(tokenId);
+        assertThat(query.getAccountId()).hasToString("0.0.123");
+        assertThat(query.getStart()).isEqualTo(5);
+        assertThat(query.getEnd()).isEqualTo(8);
     }
 }
