8e46597b40f58047dcc8def60bfd98734929e8ef
Nikita Lebedev <nikita.lebedev@limechain.tech>
2023-07-20 08:37:37 +0300

    Fixes for `release/2.26.0` (#1492)
    * Regenerated `TransactionRecordTest.snap` (unit tests are passing now)
    
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>
    
    * Force previewnet for ContractNonceInfoIntegrationTest
    
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>
    
    ---------
    
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- sdk/src/integrationTest/java/ContractNonceInfoIntegrationTest.java
+++ sdk/src/integrationTest/java/ContractNonceInfoIntegrationTest.java
@@ -14,10 +14,11 @@ import org.junit.jupiter.api.Test;
 public class ContractNonceInfoIntegrationTest {
     private static final String SMART_CONTRACT_BYTECODE = "6080604052348015600f57600080fd5b50604051601a90603b565b604051809103906000f0801580156035573d6000803e3d6000fd5b50506047565b605c8061009483390190565b603f806100556000396000f3fe6080604052600080fdfea2646970667358221220a20122cbad3457fedcc0600363d6e895f17048f5caa4afdab9e655123737567d64736f6c634300081200336080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfea264697066735822122053dfd8835e3dc6fedfb8b4806460b9b7163f8a7248bac510c6d6808d9da9d6d364736f6c63430008120033";
 
+    // revisit me later
     @Test
     @DisplayName("Contract Create of A nonce, which deploys contract B in CONSTRUCTOR")
     void canIncrementNonceThroughContractConstructor() throws Exception {
-        var testEnv = new IntegrationTestEnv(1);
+        var testEnv = new IntegrationTestEnv(1, 1);
 
         @Var var response = new FileCreateTransaction()
             .setKeys(testEnv.operatorKey)
