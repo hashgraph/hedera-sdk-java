9dee54ad0905483096b722a3eb20a07f2389e29a
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-02-20 13:37:13 +0100

    Turn integration tests into Module
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

---
+++ sdk/src/integrationTest/java/com/hedera/hashgraph/sdk/integration/test/LiveHashAddIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.hedera.hashgraph.sdk.integration.test;
+
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.LiveHashAddTransaction;
+import com.hedera.hashgraph.sdk.PrecheckStatusException;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.Status;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class LiveHashAddIntegrationTest {
+    private static final byte[] HASH = Hex.decode("100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002");
+
+    @Test
+    @DisplayName("Cannot create live hash because it's not supported")
+    void cannotCreateLiveHashBecauseItsNotSupported() throws Exception {
+        var testEnv = new IntegrationTestEnv(1);
+
+        var key = PrivateKey.generateED25519();
+
+        var response = new AccountCreateTransaction()
+            .setKey(key)
+            .setInitialBalance(new Hbar(1))
+            .execute(testEnv.client);
+
+        var accountId = Objects.requireNonNull(response.getReceipt(testEnv.client).accountId);
+
+        assertThatExceptionOfType(PrecheckStatusException.class).isThrownBy(() -> {
+            new LiveHashAddTransaction()
+                .setAccountId(accountId)
+                .setDuration(Duration.ofDays(30))
+                .setHash(HASH)
+                .setKeys(key)
+                .execute(testEnv.client)
+                .getReceipt(testEnv.client);
+        }).withMessageContaining(Status.NOT_SUPPORTED.toString());
+
+        testEnv.close(accountId, key);
+    }
+}
---
+++ sdk/src/integrationTest/java/com/hedera/hashgraph/sdk/integration/test/TopicUpdateIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.hedera.hashgraph.sdk.integration.test;
+
+import com.hedera.hashgraph.sdk.TopicCreateTransaction;
+import com.hedera.hashgraph.sdk.TopicDeleteTransaction;
+import com.hedera.hashgraph.sdk.TopicInfoQuery;
+import com.hedera.hashgraph.sdk.TopicUpdateTransaction;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TopicUpdateIntegrationTest {
+    @Test
+    @DisplayName("Can update topic")
+    void canUpdateTopic() throws Exception {
+        var testEnv = new IntegrationTestEnv(1);
+
+        var response = new TopicCreateTransaction()
+            .setAdminKey(testEnv.operatorKey)
+            .setAutoRenewAccountId(testEnv.operatorId)
+            .setTopicMemo("[e2e::TopicCreateTransaction]")
+            .execute(testEnv.client);
+
+        var topicId = Objects.requireNonNull(response.getReceipt(testEnv.client).topicId);
+
+        new TopicUpdateTransaction()
+            .clearAutoRenewAccountId()
+            .setTopicMemo("hello")
+            .setTopicId(topicId)
+            .execute(testEnv.client)
+            .getReceipt(testEnv.client);
+
+        var topicInfo = new TopicInfoQuery()
+            .setTopicId(topicId)
+            .execute(testEnv.client);
+
+        assertThat(topicInfo.topicMemo).isEqualTo("hello");
+        assertThat(topicInfo.autoRenewAccountId).isNull();
+
+        new TopicDeleteTransaction()
+            .setTopicId(topicId)
+            .execute(testEnv.client)
+            .getReceipt(testEnv.client);
+
+        testEnv.close();
+    }
+}
