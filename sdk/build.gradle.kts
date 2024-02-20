f337830cac968af740f394a52f27ccd58b28d747
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-02-20 14:16:56 +0100

    Cleanup module patching
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -11,7 +11,7 @@ moduleInfo {
     version("com.google.protobuf", "3.21.9")
     version("grpc.protobuf.lite", "1.46.0")
     version("headlong", "10.0.0")
-    version("io.grpc", "1.58.0")
+    version("io.grpc", "3.2.1")
     version("io.grpc.stub", "1.58.0")
     version("java.annotation", "1.3.2")
     version("org.bouncycastle.pkix", "1.76")
@@ -19,10 +19,13 @@ moduleInfo {
     version("org.slf4j", "2.0.9")
     version("org.slf4j.simple", "2.0.9")
 
-    version("org.assertj.core", "3.24.2")
+    // Versions only required for tests and examples
+    version("com.fasterxml.jackson.core", "2.16.1")
     version("java.dotenv", "5.3.1")
     version("json.snapshot", "1.0.17")
     version("kotlin.stdlib", "1.9.22")
+    version("org.assertj.core", "3.24.2")
+    version("org.apache.commons.lang3", "3.14.0")
 }
 
 testModuleInfo {
