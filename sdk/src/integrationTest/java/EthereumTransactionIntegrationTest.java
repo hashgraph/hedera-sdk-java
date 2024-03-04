3af7ad0d47243ad46ea81e5e5629f169c48e9217
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-03-04 16:27:57 +0200

    test: EthereumTransactionIntegrationTest revert
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- sdk/src/integrationTest/java/EthereumTransactionIntegrationTest.java
+++ sdk/src/integrationTest/java/EthereumTransactionIntegrationTest.java
@@ -16,7 +16,6 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Objects;
 import org.bouncycastle.util.encoders.Hex;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
