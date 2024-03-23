9d112c334671497ba2e7fd0eb25a241ee4ad84e2
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-03-23 18:59:36 +0200

    fix: updated example and integration tests
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- examples/src/main/java/UpdateNftsMetadataExample.java
+++ examples/src/main/java/UpdateNftsMetadataExample.java
@@ -18,21 +18,25 @@
  *
  */
 
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
 import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.NftId;
 import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.TokenAssociateTransaction;
 import com.hedera.hashgraph.sdk.TokenCreateTransaction;
 import com.hedera.hashgraph.sdk.TokenId;
+import com.hedera.hashgraph.sdk.TokenInfoQuery;
 import com.hedera.hashgraph.sdk.TokenMintTransaction;
 import com.hedera.hashgraph.sdk.TokenNftInfoQuery;
 import com.hedera.hashgraph.sdk.TokenType;
 import com.hedera.hashgraph.sdk.TokenUpdateNftsTransaction;
+import com.hedera.hashgraph.sdk.TransferTransaction;
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class UpdateNftsMetadataExample {
 
@@ -54,56 +58,92 @@ public class UpdateNftsMetadataExample {
         client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
         var metadataKey = PrivateKey.generateED25519();
-        var nftCount = 4;
-        var initialMetadataList = generateMetadata(new byte[]{4, 2, 0}, nftCount);
+        System.out.println("Generated metadata key: " + metadataKey);
 
-        // create a token with metadata key
-        var tokenId = Objects.requireNonNull(
-            new TokenCreateTransaction()
-                .setTokenName("ffff")
-                .setTokenSymbol("F")
-                .setTokenType(TokenType.NON_FUNGIBLE_UNIQUE)
-                .setTreasuryAccountId(OPERATOR_ID)
-                .setAdminKey(OPERATOR_KEY)
-                .setSupplyKey(OPERATOR_KEY)
-                .setMetadataKey(metadataKey)
-                .execute(client)
-                .getReceipt(client)
-                .tokenId
-        );
+        var initialMetadata = new byte[]{1};
+        var updatedMetadata = new byte[]{1, 2};
 
-        // mint tokens
-        var nftSerials = new TokenMintTransaction()
-            .setMetadata(initialMetadataList)
+        // Create a token with metadata key
+        var tokenCreateTransaction = new TokenCreateTransaction()
+            .setTokenName("Example")
+            .setTokenSymbol("E")
+            .setTokenType(TokenType.NON_FUNGIBLE_UNIQUE)
+            .setTreasuryAccountId(OPERATOR_ID)
+            .setAdminKey(OPERATOR_KEY)
+            .setSupplyKey(OPERATOR_KEY)
+            .setMetadataKey(metadataKey)
+            .freezeWith(client);
+
+        var tokenCreateResponse = tokenCreateTransaction.sign(OPERATOR_KEY).execute(client);
+        var tokenCreateReceipt = tokenCreateResponse.getReceipt(client);
+        System.out.println("Status of token create transaction: " + tokenCreateReceipt.status);
+
+        var tokenId = tokenCreateReceipt.tokenId;
+        System.out.println("Token id: " + tokenId);
+
+        var tokenInfo = new TokenInfoQuery()
             .setTokenId(tokenId)
+            .execute(client);
+
+        System.out.println("Token metadata key: " + tokenInfo.metadataKey);
+
+        var tokenMintTransaction = new TokenMintTransaction()
+            .setMetadata(List.of(initialMetadata))
+            .setTokenId(tokenId);
+
+        tokenMintTransaction.getMetadata().forEach(metadata -> {
+            System.out.println("Set metadata: " + Arrays.toString(metadata));
+        });
+
+        var tokenMintResponse = tokenMintTransaction.execute(client);
+        // Get receipt for mint token transaction
+        var tokenMintReceipt = tokenMintResponse.getReceipt(client);
+        System.out.println("Status of token mint transaction: " + tokenMintReceipt.status);
+
+        var nftSerials = tokenMintReceipt.serials;
+        // Check that metadata was set correctly
+        getMetadataList(client, tokenId, nftSerials).forEach(metadata -> {
+            System.out.println("Metadata after mint: " + Arrays.toString(metadata));
+        });
+
+        var accountCreateTransaction = new AccountCreateTransaction()
+            .setKey(OPERATOR_KEY)
+            .setMaxAutomaticTokenAssociations(10)
+	        .execute(client);
+
+	    var newAccountId = accountCreateTransaction.getReceipt(client).accountId;
+
+        new TokenAssociateTransaction()
+            .setAccountId(newAccountId)
+            .setTokenIds(Collections.singletonList(tokenId))
+            .freezeWith(client)
+            .sign(OPERATOR_KEY)
             .execute(client)
-            .getReceipt(client)
-            .serials;
+            .getReceipt(client);
 
-        // check that metadata was set correctly
-        System.out.println("Metadata after mint= " + getMetadataList(client, tokenId, nftSerials));
+        new TransferTransaction()
+            .addNftTransfer(tokenId.nft(nftSerials.get(0)), OPERATOR_ID, newAccountId)
+            .execute(client);
 
-        // update metadata of the first two minted NFTs
-        var updatedMetadata = new byte[]{6, 9};
-        nftSerials = new TokenUpdateNftsTransaction()
+        // Update nfts metadata
+        var tokenUpdateNftsTransaction = new TokenUpdateNftsTransaction()
             .setTokenId(tokenId)
-            .setSerials(nftSerials.subList(0, nftCount / 2))
+            .setSerials(nftSerials)
             .setMetadata(updatedMetadata)
-            .sign(metadataKey)
-            .execute(client)
-            .getReceipt(client)
-            .serials;
+            .freezeWith(client);
 
-        // check that metadata was updated correctly
-        System.out.println("Metadata after mint= " + getMetadataList(client, tokenId, nftSerials));
+        System.out.println("Updated metadata: " + Arrays.toString(tokenUpdateNftsTransaction.getMetadata()));
+        var tokenUpdateNftsResponse = tokenUpdateNftsTransaction.sign(metadataKey).execute(client);
+        // Get receipt for update nfts metadata transaction
+        var tokenUpdateNftsReceipt = tokenUpdateNftsResponse.getReceipt(client);
+        System.out.println("Status of token update nfts metadata transaction: " + tokenUpdateNftsReceipt.status);
+
+        // Check that metadata was updated correctly
+        getMetadataList(client, tokenId, nftSerials).forEach(metadata -> {
+            System.out.println("Metadata after update: " + Arrays.toString(metadata));
+        });
 
         client.close();
-    }
-
-    private static List<byte[]> generateMetadata(byte[] metadata, int count) {
-        return IntStream.range(0, count)
-            .mapToObj(i -> metadata.clone())
-            .collect(Collectors.toList());
     }
 
     private static List<byte[]> getMetadataList(Client client, TokenId tokenId, List<Long> nftSerials) {
