1749ecbe78c8f3b16f4d5a7618d8544fecaf81ce
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-18 17:20:17 +0200

    feat: publish versions for gRPC implementations
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("com.hedera.gradle.sdk")
 }
 
+// Define dependency constraints for gRPC implementations so that clients automatically get the correct version
+dependencies.constraints {
+    api("io.grpc:grpc-netty:1.64.0")
+    api("io.grpc:grpc-netty-shaded:1.64.0")
+    api("io.grpc:grpc-okhttp:1.64.0")
+}
+
 testModuleInfo {
     requires("json.snapshot")
     requires("org.assertj.core")
