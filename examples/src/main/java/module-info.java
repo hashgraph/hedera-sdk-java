81202f893382db9182148fc93d2d3bb1352a85ca
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-07-08 08:29:50 +0200

    fix: remove 'requires com.google.protobuf' from examples
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- examples/src/main/java/module-info.java
+++ examples/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.hedera.hashgraph.examples {
     // requires com.hedera.hashgraph.sdk.full;
 
     requires com.google.gson;
-    requires com.google.protobuf;
     requires java.dotenv;
 
     requires static com.google.errorprone.annotations;
