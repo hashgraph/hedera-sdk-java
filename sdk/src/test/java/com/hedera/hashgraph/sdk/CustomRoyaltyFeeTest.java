11fe33ca99a31afdf52832e76c8a0f08a913bad1
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-07-24 15:05:30 +0300

    chore: update copyright date (#1914)
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- sdk/src/test/java/com/hedera/hashgraph/sdk/CustomRoyaltyFeeTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/CustomRoyaltyFeeTest.java
@@ -2,7 +2,7 @@
  *
  * Hedera C++ SDK
  *
- * Copyright (C) 2020 - 2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2020 - 2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
