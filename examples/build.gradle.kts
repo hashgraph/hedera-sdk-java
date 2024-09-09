faa2191438491ea1143e9ef5448af72783f94c76
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-09-09 16:55:16 +0300

    release v2.39.0 (#1991)
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -28,6 +28,6 @@ mainModuleInfo {
 }
 
 dependencies.constraints {
-    implementation("com.hedera.hashgraph:sdk:2.38.0")
-    implementation("com.hedera.hashgraph:sdk-full:2.38.0")
+    implementation("com.hedera.hashgraph:sdk:2.39.0")
+    implementation("com.hedera.hashgraph:sdk-full:2.39.0")
 }
