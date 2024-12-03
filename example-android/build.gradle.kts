9cfcaceb62acc90b1fda709694d472992e33ae81
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-12-03 14:28:18 +0200

    chore: bump kotlin version
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- example-android/build.gradle.kts
+++ example-android/build.gradle.kts
@@ -20,6 +20,6 @@
 
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.autonomousapps.dependency-analysis") version "1.30.0"
 }
