3d047156bedd4291cd0953429b58aff98ef6471d
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-12-16 19:09:28 +0100

    build: repair running test AND testIntegration with code coverage on CI (#2144)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -10,3 +10,31 @@ dependencies {
     implementation(project(":sdk"))
     implementation("io.grpc:grpc-protobuf")
 }
+
+tasks.testCodeCoverageReport {
+    // Integrate coverage data from integration tests into the report
+    @Suppress("UnstableApiUsage")
+    val testIntegrationExecutionData =
+        configurations.aggregateCodeCoverageReportResults
+            .get()
+            .incoming
+            .artifactView {
+                withVariantReselection()
+                componentFilter { id -> id is ProjectComponentIdentifier }
+                attributes.attribute(
+                    Category.CATEGORY_ATTRIBUTE,
+                    objects.named(Category.VERIFICATION)
+                )
+                attributes.attribute(
+                    VerificationType.VERIFICATION_TYPE_ATTRIBUTE,
+                    objects.named(VerificationType.JACOCO_RESULTS)
+                )
+                attributes.attribute(
+                    TestSuiteType.TEST_SUITE_TYPE_ATTRIBUTE,
+                    objects.named(TestSuiteType.INTEGRATION_TEST)
+                )
+            }
+            .files
+
+    executionData.from(testIntegrationExecutionData)
+}
