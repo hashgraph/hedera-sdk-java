6d22371f8ae4d2072de20b9bd0fe346ac128cfb3
Naydenov <nikola.naydenov@limechain.tech>
2025-01-14 14:57:13 +0200

    fix: pr comments add missing integration tests
    Signed-off-by: Naydenov <nikola.naydenov@limechain.tech>

---
+++ sdk/src/testIntegration/java/org/hiero/sdk/test/integration/TransactionReceiptIntegrationTest.java
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+package org.hiero.sdk.test.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hiero.sdk.AccountCreateTransaction;
+import org.hiero.sdk.PrivateKey;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TransactionReceiptIntegrationTest {
+
+    @Test
+    @DisplayName("nextExchangeRate property is not null in TransactionReceipt")
+    void nextExchangeRatePropertyIsNotNullInTransactionReceipt() throws Exception {
+        try (var testEnv = new IntegrationTestEnv(1)) {
+
+            var key = PrivateKey.generateED25519();
+
+            var transaction = new AccountCreateTransaction().setKey(key).execute(testEnv.client);
+
+            var record = transaction.getRecord(testEnv.client);
+
+            var nextExchangeRate = record.receipt.nextExchangeRate;
+            assertThat(nextExchangeRate).isNotNull();
+        }
+    }
+}
