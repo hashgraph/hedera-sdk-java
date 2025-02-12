d3f5045c02a94477d4281ca3af2d8e8d37e0e01f
Roger Barker <roger.barker@swirldslabs.com>
2025-02-12 14:39:51 -0600

    Update version of gradle used in examples
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- examples/settings.gradle.kts
+++ examples/settings.gradle.kts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("org.hiero.gradle.build") version "0.3.0" }
+plugins { id("org.hiero.gradle.build") version "0.3.3" }
 
 @Suppress("UnstableApiUsage") dependencyResolutionManagement { repositories.mavenCentral() }
 
