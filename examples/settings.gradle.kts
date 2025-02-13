da263f72f077940693f251ae2da7cef68fb9ffa9
Roger Barker <roger.barker@swirldslabs.com>
2025-02-13 02:52:30 -0600

    chore: Snyk Integration (#2224)

--- examples/settings.gradle.kts
+++ examples/settings.gradle.kts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("org.hiero.gradle.build") version "0.3.0" }
+plugins { id("org.hiero.gradle.build") version "0.3.4" }
 
 @Suppress("UnstableApiUsage") dependencyResolutionManagement { repositories.mavenCentral() }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("org.hiero.gradle.build") version "0.3.1" }
+plugins { id("org.hiero.gradle.build") version "0.3.4" }
 
 rootProject.name = "hedera-sdk-java"
 
